1b5e2ee26bb9fa493168e68f4c9886b50f3bf060
Reinis Taukulis <reinis.taukulis@gmail.com>
2015-04-16 23:03:17 +0300

    [all] add push script
    Workflow: check out master, commit, execute push.sh.
    
    push.sh rebases local branch on the new master and force-pushes both
    branhces. Be careful as this can overwrite upstream changes that have
    not been pulled in!

---
+++ push.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $HOSTNAME == "reinis-desktop" ]]; then
+    branch="home-debian"
+elif [[ $HOSTNAME == "taukurei" ]]; then
+    branch="work-centos6"
+fi
+
+# Colors
+RCol='\e[0m'  # Reset
+Bld='\e[1m'   # Bold
+Red='\e[31m'  # Red
+Gre='\e[32m'  # Green
+Yel='\e[33m'  # Yellow
+Blu='\e[34m'  # Light Blue
+
+
+# Rebase local stuff on master
+echo -e "\n${Bld}${Gre}* Rebase local changes on master:${RCol}\n"
+echo -e "${Gre}git rebase master ${branch}:${RCol}"
+git rebase master $branch
+
+# Push
+echo -e "\n${Bld}${Gre}* Push:${RCol}\n"
+echo -e "${Gre}git push --force --all:${RCol}"
+git push --force --all
