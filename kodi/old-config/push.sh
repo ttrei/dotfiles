e8afccb7be3e6fa90b607f1cfff34b6e7f389123
Reinis Taukulis <reinis.taukulis@gmail.com>
2018-07-28 14:13:00 +0300

    [kodi-old-config] avoid find failure if there is an xml file in current directory

--- kodi/old-config/push.sh
+++ kodi/old-config/push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=$(find ./kodi -type f -name *xml -printf '%P\n')
+files=$(find ./kodi -type f -name '*xml' -printf '%P\n')
 
 for f in $files; do
     cmd="cp ./kodi/$f $HOME/.kodi/$f"
