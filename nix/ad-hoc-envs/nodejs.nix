d44cc471af67da93c33867155ac3d44de163e511
Reinis Taukulis <reinis.taukulis@gmail.com>
2018-10-13 15:31:39 +0300

    [nix-envs] nodejs.nix

---
+++ nix/ad-hoc-envs/nodejs.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "nodejs";
+  buildInputs = [
+    nodejs-10_x
+  ];
+  src = null;
+  shellHook = ''
+    export PATH=$HOME/.npm-global/bin:$PATH
+  '';
+}
