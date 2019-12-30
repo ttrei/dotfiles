721754b6caff06796a6bde34a630e8928c83e46c
Reinis Taukulis <reinis.taukulis@gmail.com>
2019-12-30 19:20:07 +0200

    Improve diff_deployed.sh
    Output only if there is a difference.
    Compare directories recursively.

--- diff_deployed.sh
+++ diff_deployed.sh
@@ -3,6 +3,9 @@
 find .deploy -type l -print0 | while IFS= read -r -d '' link; do
     dotfile=$(readlink -f $link)
     target="$HOME/${link#.deploy/}"
-    echo "diff $dotfile $target"
-    diff $dotfile $target
+    cmd="diff -r $dotfile $target"
+    if [ -n "$($cmd)" ]; then
+        echo -e "\n$ $cmd"
+        $cmd
+    fi
 done
