44b06b8afdb798cc133546ba815e0f15285f80e2
Reinis Taukulis <reinis.taukulis@gmail.com>
2020-02-08 06:37:16 +0200

    diff_deployed.sh: improve output when file is missing

--- diff_deployed.sh
+++ diff_deployed.sh
@@ -4,14 +4,14 @@ BGREEN='\033[1;92m'
 NC='\033[0m' # No Color
 
 SCRIPTDIR="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
-LINKDIR="$SCRIPTDIR/.deploy"
+SOURCEDIR="$SCRIPTDIR/.deploy"
 TARGETDIR="$HOME"
 
-find "$LINKDIR" -type l -print0 | while IFS= read -r -d '' link; do
+find "$SOURCEDIR" -type l -print0 | while IFS= read -r -d '' link; do
     src=$(readlink -f "$link")
-    target="$TARGETDIR/${link#$LINKDIR/}"
+    target="$TARGETDIR/${link#$SOURCEDIR/}"
     diff_cmd="diff -r $src $target"
-    diff_output="$($diff_cmd)"
+    diff_output="$($diff_cmd 2>&1)"
     if [ -n "$diff_output" ]; then
         echo -e "${BGREEN}$ $diff_cmd${NC}"
         echo -e "$diff_output"
