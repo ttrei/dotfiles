bdf8f54258c522ae95a9a5877d8a26c9560f6323
Reinis Taukulis <reinis.taukulis@tieto.com>
2020-02-20 12:44:43 +0200

    Improve vim plugin deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,11 @@ SCRIPTDIR="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
 SOURCEDIR="$SCRIPTDIR/.deploy"
 TARGETDIR="$HOME"
 
+# Different branches of this repo use different vim plugin mechanisms.
+# Those different mechanisms expect plugins in different directories.
+# Ensure that after deployment we have only those directories that we need.
+rm -rf "$TARGETDIR/.vim"
+
 cp -rL "$SOURCEDIR/." "$TARGETDIR"
 
 if [ -d "$TARGETDIR/.ssh" ]; then
