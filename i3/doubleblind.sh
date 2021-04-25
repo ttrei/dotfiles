210cf4a2620ed97cc208af8f151d4c935be6bdc6
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-04-25 12:59:51 +0300

    Iterate

--- i3/doubleblind.sh
+++ i3/doubleblind.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-N=3
+N=1
 SETUP_TIME=5 # seconds
 
+# https://freesound.org/people/thisusernameis/sounds/426888/
 beep1="$HOME/Downloads/beep1.wav"
+# https://freesound.org/people/Eponn/sounds/528867/
 beep2="$HOME/Downloads/beep2.wav"
+# https://freesound.org/people/florianreichelt/sounds/459992/
 beep3="$HOME/Downloads/beep3.wav"
 
 SEQUENCE_FILE=/var/tmp/doubleblind-sequence.txt
@@ -12,9 +15,24 @@ SEQUENCE_FILE=/var/tmp/doubleblind-sequence.txt
 rm -f "$SEQUENCE_FILE.tmp"
 for i in $(seq 1 $N)
 do
-    echo "i3" >> "$SEQUENCE_FILE.tmp"
+    echo "startx" >> "$SEQUENCE_FILE.tmp"
     echo "sway" >> "$SEQUENCE_FILE.tmp"
 done
+
+cat "$SEQUENCE_FILE.tmp" | shuf > "$SEQUENCE_FILE"
+
+setup_start_time=$(date +%s%3N) # milliseconds since epoch
+
+aplay -q "$beep1"
+
+while read -r line; do
+    aplay -q "$beep3" &
+    echo "$line"
+    sleep 1
+    #$line
+    aplay -q "$beep2" &
+    sleep 1
+done <"$SEQUENCE_FILE"
 
 # prompt to shut off display and press ENTER
 # record current time and wm, write to tmp file
@@ -25,9 +43,3 @@ done
 #   wait until SETUP_TIME elapsed
 #   beep to signal that setup has finished
 
-cat "$SEQUENCE_FILE.tmp" | shuf > "$SEQUENCE_FILE"
-
-setup_start_time=$(date +%s%3N) # milliseconds since epoch
-wm="i3"
-
-aplay -q "$beep1"
