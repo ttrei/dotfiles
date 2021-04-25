b4c96c623d729d29b3234621846306357147136f
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-04-25 14:39:46 +0300

    beep when setup finished

--- i3/setup-testing-env.sh
+++ i3/setup-testing-env.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
+beep3="$HOME/Downloads/beep3.wav"
+
 sleep 1
 windowid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 winid="$(printf "%u\n" $windowid 2>/dev/null)"
 i3-msg -q [id=\"$winid\"] "focus; fullscreen"
+
+aplay -q "$beep3" &
 bash
