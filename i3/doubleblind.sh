252afed2630cacdac76410380501f5da2df654ff
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-04-25 18:41:33 +0300

    wait also on teardown

--- i3/doubleblind.sh
+++ i3/doubleblind.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 N=8
+SETUP_TIME=2 # seconds
 
 # https://freesound.org/people/thisusernameis/sounds/426888/
 beep1="$HOME/Downloads/beep1.wav"
 # https://freesound.org/people/Eponn/sounds/528867/
-beep2="$HOME/Downloads/beep2.wav"
+# beep2="$HOME/Downloads/beep2.wav"
+# https://freesound.org/people/florianreichelt/sounds/459992/
+beep3="$HOME/Downloads/beep3.wav"
+
 
 RESULTS_FILE=/var/tmp/doubleblind-results.txt
 SEQUENCE_FILE=/var/tmp/doubleblind-sequence.txt
-SETUP_TIMESTAMP_FILE=/tmp/doubleblind-testcase-setup-starttime.txt
+SETUP_TIMESTAMP_FILE=/tmp/doubleblind-testcase-timestamp.txt
 
 # Generate the testing sequence
 rm -f "$SEQUENCE_FILE.tmp"
@@ -31,9 +35,11 @@ shuf "$SEQUENCE_FILE.tmp" > "$SEQUENCE_FILE"
 # Perform testing
 rm -f "$RESULTS_FILE"
 while read -r line; do
-    aplay -q "$beep1" &
-    read -rp "Press ENTER to start next test" </dev/tty
-    date +%s%3N > "$SETUP_TIMESTAMP_FILE" # milliseconds since epoch
+    read -rp "Turn display off, press ENTER, wait for beep, turn display on" </dev/tty
+
+    setup_finish_target_time=$(echo "$(date +%s%3N) + $SETUP_TIME*1000" | bc) # milliseconds since epoch
+    echo "$setup_finish_target_time" > "$SETUP_TIMESTAMP_FILE"
+
     echo "$line" >> "$RESULTS_FILE"
 
     # setup-testing-env.sh will be executed on startup
@@ -44,6 +50,16 @@ while read -r line; do
         sway 1>/dev/null 2>&1
     fi
 
+    teardown_finish_target_time=$(cat $SETUP_TIMESTAMP_FILE)
+    currenttime=$(date +%s%3N)
+    timediff=$(echo "$teardown_finish_target_time - $currenttime" | bc) # milliseconds
+    if [[ "$timediff" -gt 0 ]]; then
+        sleeptime=$(echo "scale=3; $timediff/1000" | bc -l) # seconds
+        echo "sleep $sleeptime"
+        sleep "$sleeptime"
+    fi
+
+    aplay -q "$beep3" &
     while true; do
         read -rp "Your guess (1=i3, 2=sway): " guess </dev/tty
         case $guess in
@@ -52,9 +68,7 @@ while read -r line; do
             * ) echo "Please answer 1 or 2";;
         esac
     done
-
-    aplay -q "$beep2" &
-    sleep 1
+    aplay -q "$beep1" &
 done <"$SEQUENCE_FILE"
 
 echo "Results are stored in $RESULTS_FILE:"
--- i3/setup-testing-env.sh
+++ i3/setup-testing-env.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-SETUP_TIME=2 # seconds
-SETUP_TIMESTAMP_FILE=/tmp/doubleblind-testcase-setup-starttime.txt
+SETUP_TIMESTAMP_FILE=/tmp/doubleblind-testcase-timestamp.txt
+TEARDOWN_TIME=2 # seconds
 
-starttime=$(cat $SETUP_TIMESTAMP_FILE)
-targettime=$(echo "$starttime + $SETUP_TIME*1000" | bc)
+# https://freesound.org/people/florianreichelt/sounds/459992/
+beep3="$HOME/Downloads/beep3.wav"
+
+setup_finish_target_time=$(cat $SETUP_TIMESTAMP_FILE)
 currenttime=$(date +%s%3N)
-timediff=$(echo "$targettime - $currenttime" | bc) # milliseconds
+timediff=$(echo "$setup_finish_target_time - $currenttime" | bc) # milliseconds
 if [[ "$timediff" -gt 0 ]]; then
     sleeptime=$(echo "scale=3; $timediff/1000" | bc -l) # seconds
     sleep "$sleeptime"
@@ -16,7 +18,11 @@ windowid=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
 winid="$(printf "%u\n" "$windowid" 2>/dev/null)"
 i3-msg -q [id="$winid"] "focus; fullscreen"
 
-# https://freesound.org/people/florianreichelt/sounds/459992/
-aplay -q "$HOME/Downloads/beep3.wav" &
+aplay -q "$beep3" &
 
-bash
+read -rp "Turn display off and press ENTER to end test" </dev/tty
+
+teardown_finish_target_time=$(echo "$(date +%s%3N) + $TEARDOWN_TIME*1000" | bc) # milliseconds since epoch
+echo "$teardown_finish_target_time" > "$SETUP_TIMESTAMP_FILE"
+
+i3-msg exit
