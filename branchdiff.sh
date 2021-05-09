356373ec3a3d0706e73572ee3bc38e389397a07f
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-05-09 12:04:57 +0300

    Show only differences in branchdiff; add some colors

--- branchdiff.sh
+++ branchdiff.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 RED='\033[0;31m'
+BRED='\033[0;91m'
 GREEN='\033[0;32m'
 BGREEN='\033[1;92m'
+BBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 [ -z "$1" ] && echo "Please specify the path that you want to diff" && exit 1
@@ -16,9 +18,13 @@ function show_diff() {
 
     for dep in "${deps[@]}"
     do
-        command="git diff --stat $dep..$branch -- $PATHSPEC"
-        echo $command
-        $command
+        command="git diff --stat $dep $branch -- $PATHSPEC"
+        color_command="git diff --stat ${BBLUE}$dep${NC} ${BRED}$branch${NC} -- $PATHSPEC"
+        diff_output="$($command 2>&1)"
+        if [ -n "$diff_output" ]; then
+            echo -e "$color_command"
+            $command
+        fi
     done
 }
 
