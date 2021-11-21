17d669e3a57a1908ec744f4454465926239ad9bb
Reinis Taukulis <reinis.taukulis@gmail.com>
2021-11-21 12:26:39 +0200

    [i3] Workspace script for browser

---
+++ i3/workspace-scripts/workspace-home-browser.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+from i3ipc import Connection
+from subprocess import Popen
+
+i3 = Connection()
+
+# NOTE: --no-auto-back-and-forth doesn't have the intended effect
+
+#i3.command("workspace 000:spawn")
+
+Popen(["firefox"])
+time.sleep(1.0)
+i3.command("move --no-auto-back-and-forth container to workspace 10:browser")
+
+#i3.command("workspace back_and_forth")
