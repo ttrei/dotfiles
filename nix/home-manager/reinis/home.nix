adb4e7596b040d8e590624fc7f000ba86ab5f279
Reinis Taukulis <reinis.taukulis@tietoevry.com>
2022-02-07 10:18:32 +0200

    [nix] One home-manager config for NixOS and standalone nix installations

--- nix/home-manager/reinis/home.nix
+++ nix/home-manager/reinis/home.nix
@@ -7,11 +7,30 @@
   home.homeDirectory = "/home/reinis";
 
   home.packages = with pkgs; [
+    direnv
     emacs
     fd
     fzf
+    lorri
+    git
+    git-crypt
     ripgrep
   ];
+
+  programs.bash = {
+    enable = false;
+    # initExtra = ''
+    #   # This is probably needed if we want to add extra env variables
+    #   # https://discourse.nixos.org/t/home-manager-doesnt-seem-to-recognize-sessionvariables/8488/7
+    #   . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    # '';
+    profileExtra = ''
+      source ~/.profile.legacy
+    '';
+    bashrcExtra = ''
+      source ~/.bashrc.legacy
+    '';
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
