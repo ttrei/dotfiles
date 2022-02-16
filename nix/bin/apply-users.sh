9997244593e7054cb70ba6d13da6d3f72ee01aa3
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-02-17 00:05:23 +0200

    [nix] Install xow only on kodi

--- nix/bin/apply-users.sh
+++ nix/bin/apply-users.sh
@@ -4,7 +4,9 @@ pushd ~/dotfiles/nix/home-manager
 
 home-manager switch -f ./reinis/home.nix
 
-echo "Installing xow"
-./reinis/overlays/pkgs/myxow/install-xow.sh
+if [ "$HOSTNAME" = "kodi-new" ]; then
+    echo "Installing xow"
+    ./reinis/overlays/pkgs/myxow/install-xow.sh
+fi
 
 popd
