531d03f32b7f4856a2bd2d0edb46b11a4e006045
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-02-22 22:48:08 +0200

    Add script for autoformatting python code; do it now

--- i3/workspace-scripts/workspace-home-browser.py
+++ i3/workspace-scripts/workspace-home-browser.py
@@ -10,10 +10,10 @@ i3 = Connection()
 
 # NOTE: --no-auto-back-and-forth doesn't have the intended effect
 
-#i3.command("workspace 000:spawn")
+# i3.command("workspace 000:spawn")
 
 Popen(["firefox"])
 time.sleep(3.0)
 i3.command("move --no-auto-back-and-forth container to workspace 10:browser")
 
-#i3.command("workspace back_and_forth")
+# i3.command("workspace back_and_forth")
--- other/bin/get_upgrade_counts.py
+++ other/bin/get_upgrade_counts.py
@@ -6,17 +6,18 @@ import os
 import socket
 
 logging.basicConfig(
-    filename='/var/tmp/upgrade_counts.json.log',
+    filename="/var/tmp/upgrade_counts.json.log",
     level=logging.ERROR,
-    format='%(asctime)s (%(levelname)s): %(message)s'
+    format="%(asctime)s (%(levelname)s): %(message)s",
 )
+
 
 def main():
     summary = []
-    entry = {'host': socket.gethostname()}
-    if os.path.exists('/var/tmp/upgradable_packages.txt'):
+    entry = {"host": socket.gethostname()}
+    if os.path.exists("/var/tmp/upgradable_packages.txt"):
         count = 0
-        with open('/var/tmp/upgradable_packages.txt', 'r') as f:
+        with open("/var/tmp/upgradable_packages.txt", "r") as f:
             lines = f.readlines()
         for line in lines:
             if line.startswith("Listing"):
@@ -28,12 +29,13 @@ def main():
             if line.startswith("these paths will be fetched"):
                 continue
             count += 1
-        entry['count'] = str(count)
+        entry["count"] = str(count)
         summary.insert(0, entry)
 
-    with open('/var/tmp/upgrade_counts.json.new', 'w') as f:
+    with open("/var/tmp/upgrade_counts.json.new", "w") as f:
         f.write(json.dumps(summary))
-        os.rename('/var/tmp/upgrade_counts.json.new', '/var/tmp/upgrade_counts.json')
+        os.rename("/var/tmp/upgrade_counts.json.new", "/var/tmp/upgrade_counts.json")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
