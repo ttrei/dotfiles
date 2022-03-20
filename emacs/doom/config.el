5b700d79f795f9bd0034c776bc7758d9fda7f98b
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-03-20 21:46:08 +0200

    [emacs] Record the time when note was marked as done

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -31,6 +31,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; Record the time when note was marked as done
+(setq org-log-done 'time)
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
