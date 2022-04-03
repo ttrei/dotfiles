b42d7f3662f3e70d0cb308b5b2e659ba5b353445
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-04-03 20:06:52 +0300

    Separate staging and deployment scripts
    This way I can diff current state of $HOME against staged dotfiles.

--- diff_deployed.sh
+++ diff_deployed.sh
@@ -3,13 +3,15 @@
 BGREEN='\033[1;92m'
 NC='\033[0m' # No Color
 
-SCRIPTDIR="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
-SOURCEDIR=${SOURCEDIR:-"$SCRIPTDIR/.deploy"}
+DOTFILES="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
+STAGINGDIR="$DOTFILES/.staging"
 TARGETDIR=${TARGETDIR:-"$HOME"}
 
-find "$SOURCEDIR" -type l -print0 | while IFS= read -r -d '' link; do
-    src=$(readlink -f "$link")
-    target="$TARGETDIR/${link#$SOURCEDIR/}"
+"$DOTFILES/stage.sh"
+
+find "$STAGINGDIR" -type f -print0 | while IFS= read -r -d '' dotfile; do
+    src=$(readlink -f "$dotfile")
+    target="$TARGETDIR/${dotfile#"$STAGINGDIR/"}"
     diff_cmd="diff -x __pycache__ -r $src $target"
     diff_output="$($diff_cmd 2>&1)"
     if [ -n "$diff_output" ]; then
