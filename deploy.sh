7ab864dac43eb208465f3d6802521a5c716f1806
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-04-03 18:22:33 +0300

    Introduce staging directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-SCRIPTDIR="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
+DOTFILES="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
+STAGINGDIR="$DOTFILES/.staging"
+TARGETDIR=${TARGETDIR:-"$HOME"}
 
 print_env() {
     echo "DISTRO=$DISTRO"
@@ -17,6 +19,14 @@ if [ -z "$DISTRO" ] || [ -z "$CONTEXT" ] || [ -z "$MACHINE" ] || [ -z "$EXECUTIO
     exit 1
 fi
 
+rm -rf "$STAGINGDIR"
+echo "Staging to $STAGINGDIR"
+
+
+################################################################################
+# PREPARE DEPLOYMENT (old style, to be gradually migrated to new style)
+################################################################################
+
 if [ "$DISTRO" = "debian" ] && [ "$CONTEXT" = "home" ] && [ "$MACHINE" = "home-desktop" ]; then
     OLD_DEPLOYMENT_NAME="home-desktop-debian"
 elif [ "$DISTRO" = "debian" ] && [ "$CONTEXT" = "work" ] && [ "$MACHINE" = "home-desktop" ]; then
@@ -28,49 +38,44 @@ elif [ "$DISTRO" = "ubuntu" ] && [ "$CONTEXT" = "home" ] && [ "$MACHINE" = "htpc
 elif [ "$DISTRO" = "debian" ] && [ "$CONTEXT" = "home" ] && [ "$MACHINE" = "taukulis.lv" ]; then
     OLD_DEPLOYMENT_NAME="debian"
 else
-    echo "ABORT! Couldn't select the old deployment directory."
+    echo "ABORT! Couldn't select the old-style deployment directory."
     echo ""
     print_env
     exit 1
 fi
-
-SOURCEDIR=${SOURCEDIR:-"$SCRIPTDIR/.deploy-$OLD_DEPLOYMENT_NAME"}
-TARGETDIR=${TARGETDIR:-"$HOME"}
-echo "Deploying from $SOURCEDIR to $TARGETDIR"
-
-if ! [ -d "$SOURCEDIR" ]; then
-    echo "ABORT! Source directory $SOURCEDIR not found."
+OLD_DEPLOYMENT_DIR="$DOTFILES/.deploy-$OLD_DEPLOYMENT_NAME"
+echo "Selected old-style deployment directory: $OLD_DEPLOYMENT_DIR"
+if ! [ -d "$OLD_DEPLOYMENT_DIR" ]; then
+    echo "ABORT! Old-style deployment directory $OLD_DEPLOYMENT_DIR not found."
     exit 1
 fi
 
-# Different branches of this repo use different vim plugin mechanisms.
-# Those different mechanisms expect plugins in different directories.
-# Ensure that after deployment we have only those directories that we need.
-rm -rf "$TARGETDIR/.vim"
-
-rm -rf "$TARGETDIR/bin/i3/workspace-scripts"
-
 cp --recursive \
     --dereference \
-    --remove-destination \
-    "$SOURCEDIR/." "$TARGETDIR"
+    "$OLD_DEPLOYMENT_DIR/." "$STAGINGDIR"
 
-# git does not allow .gitignore symlinks anymore:
-# https://github.com/git/git/blob/master/Documentation/RelNotes/2.32.0.txt
-# And it was ambiguous anyway - git thought that I'm using it to ignore stuff in
-# the .deploy directory.
-# Copy .gitignore explicitly.
-cp "$SCRIPTDIR/git/gitignore" "$TARGETDIR/.gitignore"
 
-if [ -d "$TARGETDIR/.ssh" ]; then
-    chmod 700 "$TARGETDIR/.ssh"
+################################################################################
+# PREPARE DEPLOYMENT (new style)
+################################################################################
+
+# GIT ######################
+
+cp "$DOTFILES/git/gitignore" "$STAGINGDIR/.gitignore"
+
+# SSH ######################
+
+if [ -d "$STAGINGDIR/.ssh" ]; then
+    chmod 700 "$STAGINGDIR/.ssh"
 fi
-if [ -f "$TARGETDIR/.ssh/config" ]; then
-    chmod 600 "$TARGETDIR/.ssh/config"
+if [ -f "$STAGINGDIR/.ssh/config" ]; then
+    chmod 600 "$STAGINGDIR/.ssh/config"
 fi
 
-if [ -d "$TARGETDIR/.config/i3/config.d" ]; then
-    i3dir="$TARGETDIR/.config/i3"
+# I3/SWAY ##################
+
+if [ -d "$STAGINGDIR/.config/i3/config.d" ]; then
+    i3dir="$STAGINGDIR/.config/i3"
     cp "$i3dir/config.d/config" "$i3dir/config"
 
     if test -n "$(find "$i3dir/config.d" -maxdepth 1 -name 'config-*' -print -quit)"
@@ -80,3 +85,18 @@ if [ -d "$TARGETDIR/.config/i3/config.d" ]; then
     fi
 
 fi
+
+
+################################################################################
+# DEPLOY
+################################################################################
+
+echo "DEPLOYING $STAGINGDIR --> $TARGETDIR"
+
+rm -rf "$TARGETDIR/.vim"
+
+rm -rf "$TARGETDIR/bin/i3/workspace-scripts"
+
+cp --recursive \
+    --remove-destination \
+    "$STAGINGDIR/." "$TARGETDIR"
