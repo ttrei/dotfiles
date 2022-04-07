abd821ce8c325e290c87f108f688cd686da4c5a5
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-04-07 22:35:27 +0300

    [i3] Improve the workspace initialization script
    All configuration - program launch parameters and window handling
    commands - is now together in one dictionary.

--- i3/workspace-scripts/workspace-home-init.py
+++ i3/workspace-scripts/workspace-home-init.py
@@ -1,68 +1,77 @@
 #!/usr/bin/env python3
 import asyncio
+from typing import Set
 
 from i3ipc import Event
 from i3ipc.aio import Connection
 from i3ipc.events import WindowEvent
 
-WORKSPACES = {
-    "05:notes": 2,
-    "10:browser": 1,
-    "20:dev": 2,
+from program import Program, construct_workspace_programs
+
+TIMEOUT = 5.0
+
+WORKSPACE_PROGRAMS = {
+    # workspace
+    "05:notes": [
+        # execstr, window_name, window_class, window_handling_commands
+        ["emacs", "emacs-notes", "emacs", ["move right"]],
+        ["zutty", "zutty-notes", "zutty", None],
+    ],
+    "10:browser": [
+        ["firefox", "mozilla firefox", "firefox", None],
+    ],
+    "20:dev": [
+        ["zutty", "zutty-dev", "zutty", None],
+        ["zutty", "zutty-dev", "zutty", None],
+    ],
 }
+
+STARTING_PROGRAMS: Set[Program] = set()
 
 
 async def on_new_window(i3: Connection, e: WindowEvent):
     _ = i3
-    name = e.container.name
+    window_name = e.container.name
     window_class = e.container.window_class
 
-    print(f"on_new_window: {name=}, {window_class=}")
+    print(f"on_new_window: {window_name=}, {window_class=}")
 
-    if name in {"emacs-notes", "zutty-notes"}:
-        workspace = "05:notes"
-    elif window_class.lower() == "firefox":
-        workspace = "10:browser"
-    elif name == "zutty-dev":
-        workspace = "20:dev"
-    else:
+    matched_program = None
+    for program in STARTING_PROGRAMS:
+        if program.match(window_name, window_class):
+            matched_program = program
+            break
+    if matched_program is None:
         return
 
-    await e.container.command(f"move to workspace {workspace}")
+    STARTING_PROGRAMS.remove(matched_program)
+    await e.container.command(f"move to workspace {matched_program.workspace}")
+    for command in matched_program.window_handling_commands:
+        await e.container.command(command)
 
-    if name == "emacs-notes":
-        await e.container.command("move right")
-
-    global WORKSPACES
-    WORKSPACES[workspace] -= 1
-    if WORKSPACES[workspace] == 0:
-        WORKSPACES.pop(workspace)
-
-    if len(WORKSPACES) == 0:
-        # await asyncio.sleep(1)
+    if len(STARTING_PROGRAMS) == 0:
         i3.main_quit()
 
 
 async def main():
     i3 = await Connection(auto_reconnect=True).connect()
-    global WORKSPACES
+
     # We will not touch already existing workspaces
     existing_workspaces = {w.name for w in await i3.get_workspaces()}
-    WORKSPACES = {k: v for k, v in WORKSPACES.items() if k not in existing_workspaces}
-    if len(WORKSPACES) == 0:
+
+    for workspace, programs in construct_workspace_programs(WORKSPACE_PROGRAMS).items():
+        if workspace in existing_workspaces:
+            continue
+        for program in programs:
+            await asyncio.create_subprocess_exec(*program.get_exec_tuple())
+            STARTING_PROGRAMS.add(program)
+
+    if len(STARTING_PROGRAMS) == 0:
         return
 
-    if "05:notes" in WORKSPACES:
-        await asyncio.create_subprocess_exec("emacs", "--title=emacs-notes")
-        await asyncio.create_subprocess_exec("zutty", "-t", "zutty-notes")
-    if "10:browser" in WORKSPACES:
-        await asyncio.create_subprocess_exec("firefox")
-    if "20:dev" in WORKSPACES:
-        await asyncio.create_subprocess_exec("zutty", "-t", "zutty-dev")
-        await asyncio.create_subprocess_exec("zutty", "-t", "zutty-dev")
-
     i3.on(Event.WINDOW_NEW, on_new_window)
-    await i3.main()
+
+    await asyncio.wait_for(i3.main(), timeout=TIMEOUT)
 
 
 asyncio.run(main())
