d86f12c5e1e46e1bdd5b97b6d61e2580800600f8
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-04-16 17:08:37 +0300

    [i3] Example no how to launch vscode

--- i3/workspace-scripts/test.py
+++ i3/workspace-scripts/test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import os
+
 import initworkspace
 
 TIMEOUT = 2.0
+
+HOME = os.path.expanduser("~")
 
 WORKSPACE_PROGRAMS = {
     # workspace
     "100:test": [
         # execstr, window_name, window_class, window_handling_commands
         ["zutty -e sleep 1", "sleep", "zutty", None],
+        # [f"code {HOME}/.config/vscode-workspaces/ziglings.code-workspace", "visual studio code", "code", None],
+        # [f"code {HOME}/.config/vscode-workspaces/algorithms.code-workspace", "visual studio code", "code", None],
     ],
 }
 
