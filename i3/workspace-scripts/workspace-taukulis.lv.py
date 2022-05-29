665809d52b27dbde1af7af610c3135ab2a3ea788
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-05-29 11:55:57 +0300

    [i3] Add workspace for taukulis.lv development

---
+++ i3/workspace-scripts/workspace-taukulis.lv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+
+import initworkspace
+
+TIMEOUT = 5.0
+
+HOME = os.path.expanduser("~")
+DIR = f"{HOME}/dev/projects/taukulis.lv"
+VSCODE_WORKSPACE = f"{HOME}/.config/vscode-workspaces/taukulis.lv.code-workspace"
+
+WORKSPACE_PROGRAMS = {
+    "20:dev": [
+        [f"exec-in-dir {DIR} zutty", "zutty-pool", "zutty", None],
+        [f"exec-in-dir {DIR} zutty", "zutty-pool", "zutty", None],
+    ],
+    "25:vscode": [
+        [f"code {VSCODE_WORKSPACE}", "visual studio code", "code", None],
+    ],
+}
+
+initworkspace.run(WORKSPACE_PROGRAMS, TIMEOUT)
