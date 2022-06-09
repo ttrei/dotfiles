9a8adedf3a3efb46d25984b6102fc0d2bf0f2a6c
Reinis Taukulis <reinis.taukulis@tietoevry.com>
2022-06-09 20:47:43 +0300

    [deps] split the script

--- dependencies/install-doom-emacs.sh
+++ dependencies/install-doom-emacs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-cargo install $(cat rust-tools.txt)
-
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 DOOMDIR=~/.config/doom ~/.emacs.d/bin/doom install
 sudo apt install markdown # doom emacs needs a markdown compiler
