8cfa5a3a164e56e3ae591a26e56dada4e5371b19
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-06-09 20:46:14 +0300

    [deps] doom emacs needs a markdown compiler

--- dependencies/install-dependencies.sh
+++ dependencies/install-dependencies.sh
@@ -4,3 +4,4 @@ cargo install $(cat rust-tools.txt)
 
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 DOOMDIR=~/.config/doom ~/.emacs.d/bin/doom install
+sudo apt install markdown # doom emacs needs a markdown compiler
