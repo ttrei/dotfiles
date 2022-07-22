9ecbebc0b78ecf0a6dd080a0b6c3cd96f4c8ade6
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-07-22 23:28:30 +0300

    [nix] Install nix-direnv

--- nix/home-manager/reinis/home.nix
+++ nix/home-manager/reinis/home.nix
@@ -45,6 +45,11 @@
     '';
   };
 
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
