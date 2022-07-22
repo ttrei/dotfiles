0b40a3e49d9a3b80279dd4effd4b79596bfe8fca
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-07-23 00:00:46 +0300

    [terminal][nix] Move direnvrc to home.nix

--- nix/home-manager/reinis/home.nix
+++ nix/home-manager/reinis/home.nix
@@ -46,6 +46,21 @@
 
   programs.direnv = {
     enable = true;
+    stdlib = ''
+      # https://github.com/direnv/direnv/wiki/Customizing-cache-location
+      # Two things to know:
+      # * `direnv_layour_dir` is called once for every {.direnvrc,.envrc} sourced
+      # * The indicator for a different direnv file being sourced is a different `''$PWD` value
+      # This means we can hash `''$PWD` to get a fully unique cache path for any given environment
+      : ''${XDG_CACHE_HOME:=''$HOME/.cache}
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              echo -n "''$XDG_CACHE_HOME"/direnv/layouts/
+              echo -n "''$PWD" | sha1sum | cut -d ' ' -f 1
+          )}"
+      }
+    '';
     nix-direnv.enable = true;
   };
 
