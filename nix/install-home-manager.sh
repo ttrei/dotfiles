661ddc63896930cac2c35cc6cf90e79cfbf17e34
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-04 21:06:03 +0300

    [nix][terminal] Fixup nix installation scripts

---
+++ nix/install-home-manager.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# https://nix-community.github.io/home-manager/index.html#sec-install-standalone
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
