9f9193ecaca0f760f30eccd49f60d0dc383ef975
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-04 21:06:03 +0300

    [nix][terminal] Fixup nix installation scripts

--- nix/install-nix.sh
+++ nix/install-nix.sh
@@ -1,15 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Installs nix and home-manager.
-# To be used on non-NixOS distributions.
+# To be used on non-NixOS distributions
 
-# nix
 # https://nixos.org/download.html#download-nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
-
-# home-manager
-# https://nix-community.github.io/home-manager/index.html#sec-install-standalone
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz
-nix-channel --update
-export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-nix-shell '<home-manager>' -A install
