142b01f4896318c46260f536e5daca1ba024ae1e
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-04 21:51:28 +0300

    Fixup scripts

--- dependencies/install-i3blocks.sh
+++ dependencies/install-i3blocks.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -q -y automake autotools-dev
+sudo apt-get install -q -y automake autotools-dev
 git clone https://github.com/vivien/i3blocks /tmp/i3blocks
 pushd /tmp/i3blocks
 ./autogen.sh
@@ -13,5 +13,5 @@ make
 sudo make install
 popd
 rm -rf /tmp/i3blocks
-sudo DEBIAN_FRONTEND=noninteractive apt-get remove --purge -q -y automake autotools-dev
-sudo DEBIAN_FRONTEND=noninteractive apt-get autopurge -q -y
+sudo apt-get remove --purge -q -y automake autotools-dev
+sudo apt-get autopurge -q -y
