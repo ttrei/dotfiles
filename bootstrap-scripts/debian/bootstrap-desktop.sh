c06584218d99e4267417a8717da8fcedf44ef9d5
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-06 20:24:39 +0300

    Update comment

--- bootstrap-scripts/debian/bootstrap-desktop.sh
+++ bootstrap-scripts/debian/bootstrap-desktop.sh
@@ -17,10 +17,13 @@ LOGFILE="$HOME/bootstrap_$(date +%FT%H-%M-%S).log"
 echo -e "Output will be written to logfile:\n$LOGFILE"
 read -rp "Press Enter to continue" </dev/tty
 
-mkdir -p /lib/firmware/amdgpu/
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git \
-    /tmp/linux-firmware
-cp /tmp/linux-firmware/amdgpu/vega20_*.bin /lib/firmware/amdgpu/
+# NOTE: Not sure if this is necessary anymore.
+#       And if it is, then only on baremetal installations.
+#       I could use the ~/.dotfiles-env to detect if this is baremetal install.
+# mkdir -p /lib/firmware/amdgpu/
+# git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git \
+#     /tmp/linux-firmware
+# cp /tmp/linux-firmware/amdgpu/vega20_*.bin /lib/firmware/amdgpu/
 
 # `bash -x` needed to record commands+output of the called script
 bash -x scripts/install_base.sh 2>&1 | tee "$LOGFILE"
