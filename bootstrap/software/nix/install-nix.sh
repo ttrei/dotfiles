43ad72b275cd606529c0ba1610d0b7e839105254
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-06 21:36:42 +0300

    Update bootstrap scripts

--- bootstrap/software/nix/install-nix.sh
+++ bootstrap/software/nix/install-nix.sh
@@ -2,5 +2,10 @@
 
 # To be used on non-NixOS distributions
 
+if [ -x "$(command -v nix)" ]; then
+    echo "Nix already installed"
+    exit 1
+fi
+
 # https://nixos.org/download.html#download-nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
