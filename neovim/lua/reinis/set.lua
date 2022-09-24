c33c1615823fec20878c987354af766f4edbeedb
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-24 22:34:16 +0300

    [nvim] Add commented out signcolumn option

--- neovim/lua/reinis/set.lua
+++ neovim/lua/reinis/set.lua
@@ -13,10 +13,10 @@ vim.opt.smartindent = true
 -- Search
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
-vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false, 'hlsearch') end)
+vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false) end)
 vim.opt.wrapscan = false
 vim.opt.ignorecase = true
-vim.keymap.set('n', '<Leader>i', function() vim_opt_toggle('ignorecase', true, false, 'ignorecase') end)
+vim.keymap.set('n', '<Leader>i', function() vim_opt_toggle('ignorecase', true, false) end)
 vim.opt.smartcase = true
 
 -- Undo, History
@@ -28,3 +28,8 @@ vim.opt.undofile = false
 vim.opt.hidden = true -- Allow hidden buffers with unsaved changes
 vim.opt.scrolloff = 8
 vim.opt.wrap = false
+
+--- Reserve a column on the left for linter hints.
+--- I will not use it yet, but might come in handy.
+-- vim.opt.signcolumn = 'yes'
+-- vim.keymap.set('n', '<Leader>c', function() vim_opt_toggle('signcolumn', 'yes', 'auto') end)
