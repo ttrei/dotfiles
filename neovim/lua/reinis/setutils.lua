ce4afc4fa39d2df13943149f48dd3b5dd98cad8f
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-24 22:20:49 +0300

    [nvim] Move options to lua

--- neovim/lua/reinis/setutils.lua
+++ neovim/lua/reinis/setutils.lua
@@ -1,12 +1,10 @@
 -- From https://www.reddit.com/r/neovim/comments/uq85hr/how_could_i_achieve_this_toggle_shortcut_in_lua/i96whcy/
-function vim_opt_toggle(opt, on, off, name)
-  local message = name
+function vim_opt_toggle(opt, on, off)
   if vim.opt[opt]:get() == off then
     vim.opt[opt] = on
-    message = message .. " Enabled"
+    vim.notify(opt .. " = " .. tostring(on))
   else
     vim.opt[opt] = off
-    message = message .. " Disabled"
+    vim.notify(opt .. " = " .. tostring(off))
   end
-  vim.notify(message)
 end
