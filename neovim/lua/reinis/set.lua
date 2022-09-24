d131925b18366174a4d6e1fb4635450a1bf5d3a1
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-24 22:20:49 +0300

    [nvim] Move options to lua

--- neovim/lua/reinis/set.lua
+++ neovim/lua/reinis/set.lua
@@ -1,14 +1,20 @@
 require('reinis.setutils')
 
--- Indenting
+-- Indent
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Search
+vim.opt.incsearch = true
 vim.opt.hlsearch = false
+vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false, 'hlsearch') end)
+vim.opt.wrapscan = false
+vim.opt.ignorecase = true
+vim.keymap.set('n', '<Leader>i', function() vim_opt_toggle('ignorecase', true, false, 'ignorecase') end)
+vim.opt.smartcase = true
 
-vim.opt.hidden = true
-
-vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false, 'HighlightSearch') end)
+vim.opt.hidden = true -- Allow hidden buffers with unsaved changes
+vim.opt.wrap = false
