11c78a5671b635f9e34aa25c37b8f232af137fa7
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-24 22:09:44 +0300

    [nvim] Move vim_opt_toggle to a separate file

--- neovim/lua/reinis/set.lua
+++ neovim/lua/reinis/set.lua
@@ -1,3 +1,5 @@
+require('reinis.setutils')
+
 -- Indenting
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -7,17 +9,6 @@ vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
 
--- From https://www.reddit.com/r/neovim/comments/uq85hr/how_could_i_achieve_this_toggle_shortcut_in_lua/i96whcy/
-local function vim_opt_toggle(opt, on, off, name)
-  local message = name
-  if vim.opt[opt]:get() == off then
-    vim.opt[opt] = on
-    message = message .. " Enabled"
-  else
-    vim.opt[opt] = off
-    message = message .. " Disabled"
-  end
-  vim.notify(message)
-end
+vim.opt.hidden = true
 
 vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false, 'HighlightSearch') end)
