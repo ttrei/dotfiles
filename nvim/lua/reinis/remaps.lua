72499a4746a357c9937327db87b30cef190c5488
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-25 19:22:20 +0300

    [nvim] Move remaps to lua; add remaps for moving tabs

--- nvim/lua/reinis/remaps.lua
+++ nvim/lua/reinis/remaps.lua
@@ -7,8 +7,14 @@ require('reinis.utils')
 -- vim.keymap.set('n', '<C-p>', function() require('fzf-lua').files() end)
 
 
-vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false) end)
-vim.keymap.set('n', '<Leader>i', function() vim_opt_toggle('ignorecase', true, false) end)
-vim.keymap.set('n', '<Leader>w', function() vim_opt_toggle('wrap', true, false) end)
+vim.keymap.set('n', '<leader>h', function() vim_opt_toggle('hlsearch', true, false) end)
+vim.keymap.set('n', '<leader>i', function() vim_opt_toggle('ignorecase', true, false) end)
+vim.keymap.set('n', '<leader>w', function() vim_opt_toggle('wrap', true, false) end)
 
-vim.keymap.set('n', '<C-p>', require('fzf-lua').files, { desc = 'Find files using fzf' })
+vim.keymap.set('n', '<c-p>', require('fzf-lua').files, { desc = 'Find files using fzf' })
+
+-- Move through tabs
+vim.keymap.set('n', '<c-l>', ':tabnext<cr>')
+vim.keymap.set('n', '<c-h>', ':tabprevious<cr>')
+vim.keymap.set('n', '<c-s-l>', ':tabmove+1<cr>')
+vim.keymap.set('n', '<c-s-h>', ':tabmove-1<cr>')
