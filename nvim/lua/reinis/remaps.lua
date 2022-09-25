c8fc6bb60d64120b193712fc5f60467b6c7c428a
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-25 15:51:03 +0300

    [nvim] Add comment about deferred remap requires

--- nvim/lua/reinis/remaps.lua
+++ nvim/lua/reinis/remaps.lua
@@ -1,5 +1,12 @@
 require('reinis.utils')
 
+-- NOTE
+-- These are equivalent, but the second approach defers the "require" until the
+-- remap is actually used, improving startup.
+-- vim.keymap.set('n', '<C-p>', require('fzf-lua').files)
+-- vim.keymap.set('n', '<C-p>', function() require('fzf-lua').files() end)
+
+
 vim.keymap.set('n', '<Leader>h', function() vim_opt_toggle('hlsearch', true, false) end)
 vim.keymap.set('n', '<Leader>i', function() vim_opt_toggle('ignorecase', true, false) end)
 vim.keymap.set('n', '<Leader>w', function() vim_opt_toggle('wrap', true, false) end)
