4feb2524561bb10e67a5359a07d710005b881ed0
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-09-26 21:27:36 +0300

    [nvim][nix] Install pyright lsp server

--- nvim/lua/reinis/lspconfig.lua
+++ nvim/lua/reinis/lspconfig.lua
@@ -49,18 +49,10 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
--- require('lspconfig')['pyright'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
--- }
--- require('lspconfig')['rust_analyzer'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
---     -- Server-specific settings...
---     settings = {
---       ["rust-analyzer"] = {}
---     }
--- }
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
 require('lspconfig')['tsserver'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
@@ -69,3 +61,12 @@ require('lspconfig')['zls'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+
+-- require('lspconfig')['rust_analyzer'].setup{
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+--     -- Server-specific settings...
+--     settings = {
+--       ["rust-analyzer"] = {}
+--     }
+-- }
