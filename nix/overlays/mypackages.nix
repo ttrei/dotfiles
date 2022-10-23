497ab2eb59517060a7c05df697d0f9959bd4eae2
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-10-23 21:04:52 +0300

    [nix] Unify nixos and home-manager overlays

--- nix/overlays/mypackages.nix
+++ nix/overlays/mypackages.nix
@@ -1,4 +1,6 @@
 self: super: {
+  mylibusb1 = super.callPackage pkgs/mylibusb1 {};
+  myxow = super.callPackage pkgs/myxow {};
   # st = super.callPackage pkgs/st-lukesmith { };
   zutty = super.callPackage pkgs/zutty {};
 }
