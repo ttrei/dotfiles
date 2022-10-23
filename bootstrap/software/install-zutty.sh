3c477853553373ff2efeddcb77c8342d562ebb53
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-10-23 21:22:54 +0300

    [nix][terminal] zutty from home-manager fails to start

--- bootstrap/software/install-zutty.sh
+++ bootstrap/software/install-zutty.sh
@@ -14,9 +14,11 @@ libgles-dev \
 libxmu-dev
 
 sudo rm -rf /tmp/zutty
-git clone https://github.com/tomscii/zutty.git /tmp/zutty
+# git clone https://github.com/tomscii/zutty.git /tmp/zutty
+git clone https://github.com/ttrei/zutty.git /tmp/zutty
 pushd /tmp/zutty
 
+git checkout c8e4c458fa34d3b79aa7757be51303ff3367ab65
 ./waf configure
 ./waf
 
