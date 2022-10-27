917d288f5420cab18231f520611a52321f3d8dd7
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-10-27 10:02:03 +0300

    Update python bootstrap script

--- bootstrap/software/python/install-python-venv.sh
+++ bootstrap/software/python/install-python-venv.sh
@@ -7,6 +7,14 @@ if [ -d ~/.venv ]; then
     exit 1
 fi
 
+# Make sure we use the system python.
+# Don't want to create a virtualenv against a python from another virtualenv -
+# it may disappear.
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd "$SCRIPT_DIR"
+direnv allow
+eval "$(direnv export bash)" || exit 1
+
 python3 -m venv --upgrade-deps ~/.venv
 . ~/.venv/bin/activate
 python -m pip install pipx
