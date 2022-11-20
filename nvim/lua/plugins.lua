83af461bb45731c33e089e98a47ac3d4219eb073
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-11-20 19:51:15 +0200

    [nix][nvim] Use nvim-treesitter plugin from nixpkgs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,21 +1,6 @@
 -- https://github.com/wbthomason/packer.nvim/tree/6afb67460283f0e990d35d229fd38fdc04063e0a#bootstrapping
 
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
 require('packer').startup(function(use)
-  use 'https://github.com/wbthomason/packer.nvim'
-
   -- TODO
   -- use 'https://github.com/ThePrimeagen/harpoon.git'
 
@@ -32,10 +17,6 @@ require('packer').startup(function(use)
   use 'https://github.com/plasticboy/vim-markdown.git'
   use 'https://github.com/ziglang/zig.vim.git'
   --]]
-  use {
-    'https://github.com/nvim-treesitter/nvim-treesitter.git',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
   use 'https://github.com/matcatc/vim-asciidoc-folding'
   use 'https://github.com/LnL7/vim-nix.git'
 
@@ -59,11 +40,4 @@ require('packer').startup(function(use)
   --   'https://github.com/nvim-telescope/telescope.nvim.git', tag = '0.1.0',
   --   requires = { {'https://github.com/nvim-lua/plenary.nvim.git'} }
   -- }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-  -- You can also sync manually with :PackerSync
 end)
--- nvim/lua/reinis/treesitter.lua
+++ nvim/lua/reinis/treesitter.lua
@@ -1,34 +1,5 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter#modules
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    "bash",
-    "c",
-    "cpp",
-    "dockerfile",
-    "java",
-    "json",
-    "lua",
-    "make",
-    "markdown",
-    "nix",
-    "org",
-    "python",
-    "sql",
-    "vim",
-    "yaml",
-    "zig",
-  },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
