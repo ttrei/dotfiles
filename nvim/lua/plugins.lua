5be9bb3574ef15ab8fe5681fc30a0c6181cde55b
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-11-24 16:35:44 +0200

    [nvim] Use vim-visual-star-search plugin
    The built in visual star search didn't work on strings containing "/"

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,10 +2,5 @@
 
 require('packer').startup(function(use)
   use 'https://github.com/matcatc/vim-asciidoc-folding'
-
-  -- I will use the built-in visual star search for now.
-  -- Revisit this decision if I find that the built-in feature lacks something.
-  -- Not sure if this plugin does anything extra.
-  -- It does use a different mapping, so it might handle a wider range of strings.
-  -- use 'https://github.com/bronson/vim-visual-star-search.git'
+  use 'https://github.com/bronson/vim-visual-star-search.git'
 end)
