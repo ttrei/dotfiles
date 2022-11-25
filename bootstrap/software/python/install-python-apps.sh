4574f85e35fa45cab08c0ad89c3123e1b95cf83d
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-11-25 22:05:34 +0200

    [terminal] Activate the main python virtualenv on login
    Allows executing the workspace scripts - the i3init library is available
    in the virtualenv.

--- bootstrap/software/python/install-python-apps.sh
+++ bootstrap/software/python/install-python-apps.sh
@@ -3,8 +3,11 @@
 set -o errexit
 
 . ~/.venv/bin/activate
+
 pipx install --force black
 pipx install --force hatch
 pipx install --force youtube-dl
 pipx install --force yt-dlp
 pipx install --force ipython
+
+python -m pip install --editable ~/dotfiles/i3/i3init
