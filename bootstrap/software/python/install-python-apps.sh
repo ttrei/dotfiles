0f94c22634fd8f2e86081247ea756304f1223f93
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-11-30 20:31:54 +0200

    [i3] Install the bleeding-edge i3ipc-python

--- bootstrap/software/python/install-python-apps.sh
+++ bootstrap/software/python/install-python-apps.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
@@ -10,4 +10,9 @@ pipx install --force youtube-dl
 pipx install --force yt-dlp
 pipx install --force ipython
 
+pushd ~/dotfiles
+git submodule update --init i3/i3init/deps/i3ipc-python
+popd
+
+python -m pip install --editable ~/dotfiles/i3/i3init/deps/i3ipc-python
 python -m pip install --editable ~/dotfiles/i3/i3init
--- bootstrap/software/python/upgrade-python-apps.sh
+++ bootstrap/software/python/upgrade-python-apps.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
 . ~/.venv/bin/activate
 pipx upgrade-all
+
+pushd ~/dotfiles/i3/i3init/deps/i3ipc-python
+git checkout master
+git pull
+popd
