2a3806f91ff04ac779a139386948c58ee88eb63d
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-03 12:22:45 +0200

    [i3] Fixup i3init test

--- i3/i3init/tests/test.py
+++ i3/i3init/tests/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-import initworkspace
+import i3init
 
 TIMEOUT = 2.0
 
@@ -21,4 +21,4 @@ WORKSPACE_PROGRAMS = {
     # ],
 }
 
-initworkspace.run(WORKSPACE_PROGRAMS, TIMEOUT)
+i3init.run(WORKSPACE_PROGRAMS, TIMEOUT)
