94eb9fa17da12ee67ad7398ef9e0863bdcc8d74b
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-16 22:11:22 +0200

    [nix] Update nix uninstall script

--- bootstrap/software/nix/uninstall-nix.sh
+++ bootstrap/software/nix/uninstall-nix.sh
@@ -7,7 +7,12 @@ if ! [ -x "$(command -v nix)" ]; then
     exit 1
 fi
 
-sudo rm -rf /etc/profile/nix.sh /etc/nix /nix ~root/.nix-profile ~root/.nix-defexpr ~root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels
+sudo rm -rf /nix /etc/nix /etc/profile/nix.sh ~root/.nix-profile ~root/.nix-defexpr ~root/.nix-channels ~/.nix-profile ~/.nix-defexpr ~/.nix-channels
+
+for i in $(seq 30001 30032); do
+  sudo userdel $i
+done
+sudo groupdel 30000
 
 # If you are on Linux with systemd, you will need to run:
 sudo systemctl stop nix-daemon.socket
