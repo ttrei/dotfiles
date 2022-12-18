0ab0324b2aa55b4d65d1448df8bc6027f9c07220
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-18 11:15:30 +0200

    [i3] Update workspace-home-init.py

--- i3/workspace-scripts/workspace-home-init.py
+++ i3/workspace-scripts/workspace-home-init.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+import os
+
 import i3init
 
 TIMEOUT = 5.0
+
+HOME = os.path.expanduser("~")
 
 WORKSPACE_PROGRAMS = {
     # workspace
     "05:notes": [
         # execstr, window_name, window_class, window_handling_commands
         ["emacs", "emacs-notes", "emacs", ["move right"]],
-        ["zutty", "zutty-notes", "zutty", None],
+        [f"exec-in-dir {HOME}/dev/notes zutty", "zutty-notes", "zutty", None],
     ],
     "10:browser": [
         ["firefox", "mozilla firefox", "firefox", None],
