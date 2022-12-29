44a3db8f48a2ded5937fd742b137796ba5321a85
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-29 21:09:53 +0200

    [nix] Use nixos home-manager config on nixos

--- nix/bin/apply-users.sh
+++ nix/bin/apply-users.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-pushd ~/dotfiles/nix/home-manager/reinis
+ENVFILE=${DOTFILES_ENVFILE:-"$HOME/.dotfiles-env"}
+if ! [ -f "$ENVFILE" ]; then
+    echo "ABORT! dotfiles env file $ENVFILE not found."
+    exit 1
+fi
+# shellcheck source=/dev/null
+. "$ENVFILE"
 
-home-manager switch -f ./non-nixos.nix
+pushd ~/dotfiles/nix/home-manager/reinis || exit
 
-popd
+if [ "$DISTRO" = "nixos" ]; then
+    home-manager switch -f ./nixos.nix
+else
+    home-manager switch -f ./non-nixos.nix
+fi
+
+popd || exit
