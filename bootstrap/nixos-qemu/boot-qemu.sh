9d6ffdb2256923712a7f2f2c801889efc0068def
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-30 12:39:15 +0200

    [nix] Move nixos qemu installation scripts from the scripts repo

---
+++ bootstrap/nixos-qemu/boot-qemu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source env.sh
+
+qemu-system-x86_64\
+    -enable-kvm\
+    -m 8G\
+    -vga virtio\
+    -display gtk,gl=on,show-cursor=on\
+    -device virtio-net-pci,netdev=net0\
+    -netdev user,id=net0,hostfwd=tcp::$QEMU_GUEST_PORT-:22\
+    -drive file="$QEMU_GUEST_IMAGE",format=raw,index=0,media=disk,if=virtio\
+    -boot order=c\
+
