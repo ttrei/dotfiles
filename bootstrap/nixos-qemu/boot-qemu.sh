8370ed800979f8f69286109996228f0eaf87c2a2
Reinis Taukulis <reinis.taukulis@gmail.com>
2022-12-30 16:38:43 +0200

    [nix] Fixup nixos qemu setup

--- bootstrap/nixos-qemu/boot-qemu.sh
+++ bootstrap/nixos-qemu/boot-qemu.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-source env.sh
+SCRIPTDIR="$( cd "$(dirname "$0")" || exit >/dev/null 2>&1 ; pwd -P )"
+
+# shellcheck disable=SC1091
+source "$SCRIPTDIR/env.sh"
 
 qemu-system-x86_64\
     -enable-kvm\
@@ -8,7 +11,7 @@ qemu-system-x86_64\
     -vga virtio\
     -display gtk,gl=on,show-cursor=on\
     -device virtio-net-pci,netdev=net0\
-    -netdev user,id=net0,hostfwd=tcp::$QEMU_GUEST_PORT-:22\
+    -netdev user,id=net0,hostfwd=tcp::"$QEMU_GUEST_PORT"-:22\
     -drive file="$QEMU_GUEST_IMAGE",format=raw,index=0,media=disk,if=virtio\
     -boot order=c\
 
