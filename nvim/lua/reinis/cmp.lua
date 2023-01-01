4d88874d42d7a7d35c20fd892aeb5676e9590fa9
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-01-01 13:12:46 +0200

    [nvim] Configure nvim-cmp mappings

--- nvim/lua/reinis/cmp.lua
+++ nvim/lua/reinis/cmp.lua
@@ -3,6 +3,14 @@
 local cmp = require'cmp'
 
 cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
   }, {
