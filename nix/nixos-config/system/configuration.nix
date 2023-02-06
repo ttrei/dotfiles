5816bacdf19f0a3420479d3dc333635723db2793
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-02-06 21:12:19 +0200

    [nix] Enable GnuPG agent

--- nix/nixos-config/system/configuration.nix
+++ nix/nixos-config/system/configuration.nix
@@ -58,13 +58,11 @@
     hashedPassword = "$6$Cg9okSaCqojkhWfc$/EOP6PeEaL5DPnjjNtG7k7.80O5X8Sc4Q2qiTQzS1n6vn.DTp3fI5dafofJzbU/MwFTnroSMg3CT8towxuUwG.";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+     enable = true;
+     pinentryFlavor = "gtk2";
+     enableSSHSupport = true;
+  };
 
   programs.bash.enableCompletion = true;
 
