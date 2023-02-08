73d0c6c5584e668f62765a4c3e4eba79db5db03f
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-02-08 19:24:02 +0200

    [xorg] Fix display detection script

--- xorg/bin/detect_displays.sh
+++ xorg/bin/detect_displays.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Detect connected displays and export their xrandr outputs as env variables
 # Adapted from https://stackoverflow.com/a/24933353/9124671
 
-tmpfile=$(mktemp)
-while read -r output hex conn; do
-    [[ -z "$conn" ]] && conn=${output%%-*}
-    echo "$(xxd -r -p <<< "$hex") $output" >> $tmpfile
-done < <(xrandr --prop | awk '
+tmp1=$(mktemp)
+xrandr --prop | awk '
     !/^[ \t]/ {
         if (output && hex) print output, hex, conn
         output=$1
@@ -23,12 +20,19 @@ done < <(xrandr --prop | awk '
     h {sub(/[ \t]+/, ""); hex = hex $0}
     /EDID.*:/ {h=1}
     END {if (output && hex) print output, hex, conn}
-    ' | sort
-)
+' | sort > "$tmp1"
 
-PRIMARY_DISPLAY=$(grep VG245 "$tmpfile" | cut -d" " -f2)
-SECONDARY_DISPLAY=$(grep S23C650 "$tmpfile" | cut -d" " -f2)
+tmp2=$(mktemp)
+while read -r output hex conn; do
+    [ -z "$conn" ] && conn=${output%%-*}
+    id=$(echo "$hex" | xxd -r -p)
+    echo "$id" "$output" >> "$tmp2"
+done < "$tmp1"
+rm "$tmp1"
+
+PRIMARY_DISPLAY=$(grep VG245 "$tmp2" | cut -d" " -f2)
+SECONDARY_DISPLAY=$(grep S23C650 "$tmp2" | cut -d" " -f2)
 export PRIMARY_DISPLAY
 export SECONDARY_DISPLAY
 
-rm "$tmpfile"
+rm "$tmp2"
