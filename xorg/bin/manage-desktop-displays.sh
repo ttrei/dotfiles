b8489b20dc817ff46fbdd38df50e1e336b0b0b58
Reinis Taukulis <reinis.taukulis@tietoevry.com>
2023-02-09 10:21:58 +0200

    [xorg] Position the X outputs correctly

--- xorg/bin/manage-desktop-displays.sh
+++ xorg/bin/manage-desktop-displays.sh
@@ -4,12 +4,20 @@
 
 set -o nounset
 
+# The output positions can be easily chosen with the arandr tool.
+#
+# I specify the framebuffer dimensions explicitly to avoid a bug where the display goes black for
+# a moment when switching from two-display to one-display configuration.
+# https://gitlab.freedesktop.org/xorg/xserver/-/issues/216
+
 if [ "$1" = "one" ]; then
-    xrandr \
+    xrandr --fb 3000x2220 \
         --output "$PRIMARY_DISPLAY" --auto --primary \
         --output "$SECONDARY_DISPLAY" --off
 elif [ "$1" = "two" ]; then
-    xrandr \
-        --output "$PRIMARY_DISPLAY" --auto --primary \
-        --output "$SECONDARY_DISPLAY" --auto --right-of "$PRIMARY_DISPLAY" --rotate left
+    xrandr --fb 3000x2220 \
+        --output "$PRIMARY_DISPLAY" --primary --mode 1920x1080 --pos 0x300 --rotate normal \
+        --output "$SECONDARY_DISPLAY" --mode 1920x1080 --pos 1920x0 --rotate left
 fi
+
+
