02789206603e2cbab2a679d98aa0dc05f5979477
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-02-17 21:47:32 +0200

    [nix] Note some reasearch directions on persistence

--- nix/home-manager/reinis/common.nix
+++ nix/home-manager/reinis/common.nix
@@ -69,6 +69,11 @@
 
   programs.firefox = {
     # https://nix-community.github.io/home-manager/options.html#opt-programs.firefox.enable
+
+    # This works, but I want my firefox profile to persist between sessions
+    # Do some research on how to persist stuff: 
+    # https://github.com/Misterio77/nix-starter-configs/tree/0537107ce41396dff1fb1dd43705a94e9120f576#try-opt-in-persistance
+    # https://github.com/Misterio77/nix-config/blob/0ecfa1af537a70bbf4f4607501073d368e31612f/home/misterio/features/desktop/common/firefox.nix#L37
     enable = false;
     profiles.reinis = {
       id = 0;
