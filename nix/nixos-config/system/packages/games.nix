848605d9c40c2943f8445ca9efb5db2306b81ba8
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-03-27 15:41:22 +0300

    todo

--- nix/nixos-config/system/packages/games.nix
+++ nix/nixos-config/system/packages/games.nix
@@ -3,6 +3,10 @@
   pkgs,
   ...
 }: {
+
+  # TODO: Add kernel module https://github.com/medusalix/xone
+  # https://github.com/NixOS/nixpkgs/blob/5f9d1bb572e08ec432ae46c78581919d837a90f6/pkgs/os-specific/linux/xone/default.nix
+
   environment.systemPackages = with pkgs; [
     lutris
   ];
