cef5b832566017782846163ca57419f4d0168577
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-03-31 11:54:52 +0300

    [nix] Radarr, Sonarr, Jackett, Bazarr, Navidrome

--- nix/nixos-config/htpc-qemu.nix
+++ nix/nixos-config/htpc-qemu.nix
@@ -2,16 +2,37 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  unstable = import <nixos-unstable> {
+    # https://github.com/NixOS/nixpkgs/issues/55366
+    # Include the nixos config when importing nixos-unstable
+    # But remove packageOverrides to avoid infinite recursion
+    config = removeAttrs config.nixpkgs.config [ "packageOverrides" ];
+  };
+in {
+
+  # https://github.com/NixOS/nixpkgs/issues/55366
+  # https://nixos.org/manual/nixos/unstable/#sec-replace-modules
+  disabledModules = [ "services/audio/navidrome.nix" ];
+
   imports = [
     ./hardware-configurations/qemu.nix
     ./packages/cli.nix
     ./packages/gui.nix
     ./packages/htpc.nix
     ./packages/games.nix
-    ./users/user.nix
     ./users/reinis.nix
+
+    # https://github.com/NixOS/nixpkgs/issues/55366
+    # https://nixos.org/manual/nixos/unstable/#sec-replace-modules
+    <nixos-unstable/nixos/modules/services/audio/navidrome.nix>
   ];
+
+  # https://github.com/NixOS/nixpkgs/issues/55366
+  # Override select packages to use the unstable channel
+  nixpkgs.config.packageOverrides = pkgs: {
+    navidrome = unstable.navidrome;
+  };
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
@@ -62,4 +83,47 @@
     rpc-whitelist = "192.168.*.* 127.0.0.1";
     rpc-whitelist-enabled = true;
   };
+
+  services.sonarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Sonarr";
+    openFirewall = true; # port: 8989
+  };
+
+  services.radarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Radarr";
+    openFirewall = true; # port: 7878
+  };
+
+  services.jackett = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Jackett";
+    openFirewall = true; # port: 9117
+  };
+
+  services.bazarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    openFirewall = true;
+    listenPort = 6767;
+  };
+
+  services.navidrome = {
+    enable = true;
+    settings = {
+      # https://www.navidrome.org/docs/usage/configuration-options/
+      Address = "127.0.0.1";
+      Port = 4533;
+      MusicFolder = "/home/reinis/music";
+    };
+  };
+
 }
--- nix/nixos-config/htpc.nix
+++ nix/nixos-config/htpc.nix
@@ -2,21 +2,59 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  unstable = import <nixos-unstable> {
+    # https://github.com/NixOS/nixpkgs/issues/55366
+    # Include the nixos config when importing nixos-unstable
+    # But remove packageOverrides to avoid infinite recursion
+    config = removeAttrs config.nixpkgs.config [ "packageOverrides" ];
+  };
+in {
+
+  # https://github.com/NixOS/nixpkgs/issues/55366
+  # https://nixos.org/manual/nixos/unstable/#sec-replace-modules
+  disabledModules = [ "services/audio/navidrome.nix" ];
+
   imports = [
-    ./hardware-configurations/htpc.nix
+    ./hardware-configurations/qemu.nix
     ./packages/cli.nix
     ./packages/gui.nix
     ./packages/htpc.nix
     ./packages/games.nix
-    ./users/user.nix
+    ./users/reinis.nix
+
+    # https://github.com/NixOS/nixpkgs/issues/55366
+    # https://nixos.org/manual/nixos/unstable/#sec-replace-modules
+    <nixos-unstable/nixos/modules/services/audio/navidrome.nix>
   ];
+
+  # https://github.com/NixOS/nixpkgs/issues/55366
+  # Override select packages to use the unstable channel
+  nixpkgs.config.packageOverrides = pkgs: {
+    navidrome = unstable.navidrome;
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "htpc-nixos";
+
+  networking.wg-quick.interfaces = {
+    wg-mullvad = {
+      autostart = false;
+      address = [ "10.64.155.123/32" ];
+      dns = [ "10.64.0.1" ];
+      privateKeyFile = "/root/wireguard-keys/mullvad/wg-mullvad.key";
+      peers = [
+        {
+          publicKey = "m4jnogFbACz7LByjo++8z5+1WV0BuR1T7E1OWA+n8h0=";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "193.138.218.130:51820";
+        }
+      ];
+    };
+  };
 
   services.transmission.enable = true;
   services.transmission.settings = {
@@ -45,4 +83,47 @@
     rpc-whitelist = "192.168.*.* 127.0.0.1";
     rpc-whitelist-enabled = true;
   };
+
+  services.sonarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Sonarr";
+    openFirewall = true; # port: 8989
+  };
+
+  services.radarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Radarr";
+    openFirewall = true; # port: 7878
+  };
+
+  services.jackett = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    dataDir = "/home/reinis/.local/share/Jackett";
+    openFirewall = true; # port: 9117
+  };
+
+  services.bazarr = {
+    enable = true;
+    group = "users";
+    user = "reinis";
+    openFirewall = true;
+    listenPort = 6767;
+  };
+
+  services.navidrome = {
+    enable = true;
+    settings = {
+      # https://www.navidrome.org/docs/usage/configuration-options/
+      Address = "127.0.0.1";
+      Port = 4533;
+      MusicFolder = "/home/reinis/music";
+    };
+  };
+
 }
