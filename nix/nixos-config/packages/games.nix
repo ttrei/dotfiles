fea7e7ee5c19a9e3311e6517dbb9528fb0481da5
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-04-01 17:15:04 +0300

    [nix] Xbox controllers work

--- nix/nixos-config/packages/games.nix
+++ nix/nixos-config/packages/games.nix
@@ -3,9 +3,6 @@
   pkgs,
   ...
 }: {
-  # TODO: Add kernel module https://github.com/medusalix/xone
-  # https://github.com/NixOS/nixpkgs/blob/5f9d1bb572e08ec432ae46c78581919d837a90f6/pkgs/os-specific/linux/xone/default.nix
-
   environment.systemPackages = with pkgs; [
     lutris
   ];
