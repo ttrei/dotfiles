b9a6d12d124c5cc19ab9801152de9347e8f80fe3
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-04-07 13:39:08 +0300

    Move nix and nixos config to flakes directory

--- nix/overlays/mypackages.nix
+++
@@ -1,6 +0,0 @@
-self: super: {
-  mylibusb1 = super.callPackage pkgs/mylibusb1 {};
-  myxow = super.callPackage pkgs/myxow {};
-  # st = super.callPackage pkgs/st-lukesmith { };
-  zutty = super.callPackage pkgs/zutty {};
-}
