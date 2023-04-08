0c6c5b80b605ce6a310eacd590771d523e22dcb3
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-04-08 20:03:45 +0300

    Override radarr version
    I had already used the development version so the data is not compatible
    with the stable version.

--- flakes/overlays/default.nix
+++ flakes/overlays/default.nix
@@ -8,12 +8,20 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
+  # See examples here:
+  # https://github.com/Misterio77/nix-config/blob/d39c4bfa163ab6ecccf2affded0a3e5ad4b8cc7b/overlays/default.nix
   modifications = final: prev: {
     bazarr = final.unstable.bazarr;
     jackett = final.unstable.jackett;
     navidrome = final.unstable.navidrome;
-    radarr = final.unstable.radarr;
     sonarr = final.unstable.sonarr;
+    radarr = final.unstable.radarr.overrideAttrs (oldAttrs: rec {
+      version = "4.4.2.6956";
+      src = prev.fetchurl {
+        url = "https://github.com/Radarr/Radarr/releases/download/v${version}/Radarr.develop.${version}.linux-core-x64.tar.gz";
+        sha256 = "sha256-DVVBJC7gGjlF9S3KI0+9kh4EzDEoWsC2jJxD8khbN2c=";
+      };
+    });
 
     # neovim 0.8.1 of nixpkgs-22.11 has some problems with the copilot plugin
     neovim-unwrapped = final.unstable.neovim-unwrapped;
