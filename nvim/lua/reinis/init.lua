9d6c04346f2cb71f1f3687690681ca89037f7782
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-04-26 23:09:07 +0300

    Basic configuration for Neorg

--- nvim/lua/reinis/init.lua
+++ nvim/lua/reinis/init.lua
@@ -2,6 +2,7 @@ require ('reinis.options')
 require ('reinis.colors')
 require ('reinis.remaps')
 require ('reinis.lspconfig')
+require ('reinis.neorg')
 require ('reinis.treesitter')
 require ('reinis.cmp')
 
