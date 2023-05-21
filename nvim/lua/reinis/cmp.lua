cfddf0b69b4fd0010ee669083ff68ff519f6404f
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-05-21 10:31:38 +0300

    Disable copilot - the trial expired

--- nvim/lua/reinis/cmp.lua
+++ nvim/lua/reinis/cmp.lua
@@ -14,7 +14,7 @@ cmp.setup({
   }),
 
   sources = {
-    { name = 'copilot', group_index = 2 },
+    -- { name = 'copilot', group_index = 2 },
     { name = 'nvim_lsp', group_index = 2 },
     { name = 'luasnip', group_index = 2 },
     { name = 'buffer', group_index = 5 },
