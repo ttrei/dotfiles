99a30e3eab0f869c2a747a1ad7246d10b6802d8d
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-09-10 22:06:11 +0300

    Update flakify-ttrei flake to support nix develop command

--- nix/templates/nix-direnv/flake.nix
+++ nix/templates/nix-direnv/flake.nix
@@ -9,12 +9,15 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = ttrei.nixpkgs.legacyPackages.${system};
       mypkgs = ttrei.mypkgs.${system};
-    in {
+    in rec {
+      # Expose the used packages as flake output, so you can enter their build environment like this:
+      #   nix develop .#zutty
+      # The output must be named "packages" for "nix develop" to recognize it.
+      packages = {
+        zutty = mypkgs.zutty;
+      };
       devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.bashInteractive
-          # mypkgs.zutty
-        ];
+        packages = [ pkgs.bashInteractive ] ++ pkgs.lib.attrsets.mapAttrsToList (name: value: value) packages;
       };
     });
 }
