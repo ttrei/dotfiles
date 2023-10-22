2c59e7c14bd5e9696f2c605ea6a221389d367df5
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-10-22 21:54:06 +0300

    Update zutty

--- nix/pkgs/zutty/default.nix
+++ nix/pkgs/zutty/default.nix
@@ -11,14 +11,14 @@
 }:
 stdenv.mkDerivation rec {
   pname = "zutty";
-  version = "0.14.0-2023-02-18";
+  version = "0.14.0-2023-09-24";
   # version = "0.13.0-patch-reinis";
 
   src = fetchgit {
     url = "https://github.com/tomszilagyi/zutty.git";
     # url = "https://github.com/ttrei/zutty.git";
-    rev = "bc6e0893e91bc0ffd08cb7d15727973de450c3d6";
-    hash = "sha256-b/q7hIi/U/GkKo+MIFX2wWnHZAy5rQGXNul3I1pxo1Q=";
+    rev = "1ff6da9b37c1e641610dd6918791ee863b35d2e0";
+    hash = "sha256-PsubFOKs29LHfqPihBSB9V73HWSNTAUR8kR/cd6OcVQ=";
     fetchSubmodules = false;
   };
 
