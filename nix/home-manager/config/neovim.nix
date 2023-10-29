d09972c941da5a62e88b4800ea66905c46720502
Reinis Taukulis <reinis.taukulis@gmail.com>
2023-10-29 13:02:05 +0200

    Enable specifig treesitter grammar plugins in neovim

--- nix/home-manager/config/neovim.nix
+++ nix/home-manager/config/neovim.nix
@@ -44,28 +44,28 @@
       neomake
       nvim-cmp
       nvim-lspconfig
-      nvim-treesitter.withAllGrammars
-      # # The following approach failed to build on 2022-11-20 with a strange error:
-      # #   error: The option `programs.neovim.plugins."[definition 1-entry 2]".__ignoreNulls' does not exist. Definition values:
-      # #   - In `/home/reinis/dotfiles/nix/home-manager/reinis/config/neovim.nix': true
-      # nvim-treesitter.withPlugins (plugins: with plugins; [
-      #   bash
-      #   c
-      #   cpp
-      #   dockerfile
-      #   java
-      #   json
-      #   lua
-      #   make
-      #   markdown
-      #   nix
-      #   org
-      #   python
-      #   sql
-      #   vim
-      #   yaml
-      #   zig
-      # ])
+      {
+        plugin = nvim-treesitter.withPlugins (plugins:
+          with plugins; [
+            bash
+            c
+            cpp
+            dockerfile
+            java
+            json
+            lua
+            make
+            markdown
+            nix
+            org
+            python
+            sql
+            vim
+            yaml
+            zig
+          ]);
+      }
+      # nvim-treesitter.withAllGrammars
       nvim-treesitter-context
       nvim-treesitter-textobjects
       vim-nix
