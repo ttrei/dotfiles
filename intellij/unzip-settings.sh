607bdc8f3b02935c5ed17d2b0add856022a455b0
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-01-05 09:17:47 +0200

    Improve storing of intellij settings

--- intellij/unzip-settings.sh
+++ intellij/unzip-settings.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 app=$1
-if [ -z "$app" ] || ([ "$app" != "idea" ] && [ "$app" != "pycharm" ]); then
+if [ -z "$app" ] || { [ "$app" != "idea" ] && [ "$app" != "pycharm" ]; }; then
     echo "Usage: $0 <idea|pycharm>"
     exit 1
 fi
@@ -14,4 +14,9 @@ if ! [ -f "settings.zip" ]; then
     exit 1
 fi
 
+if ! [ -d "$app" ]; then
+    echo "ERROR: $app is not a directory";
+    exit 1;
+fi
+rm -rf "$app"
 unzip settings.zip -d "$app"
