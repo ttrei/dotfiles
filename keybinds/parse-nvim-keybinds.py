437ccdfa991af1d608659baa57f78e9d3cf25641
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-02-04 23:16:44 +0200

    Select keybind using rofi

--- keybinds/parse-nvim-keybinds.py
+++ keybinds/parse-nvim-keybinds.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 
-def main():
+import subprocess
+
+def parse():
     maps = {
         "n": {},
         "i": {},
@@ -18,6 +20,27 @@ def main():
                 lhs = line.split()[1]
                 rhs = " ".join(line.split()[2:]).lstrip("*&@")
                 maps[prefix][lhs] = rhs
+    return maps
+
+
+def select_mapping(mapping):
+    keyvalues = []
+    for key, value in mapping.items():
+        keyvalues.append(f"{key}: {value}")
+    command = ['rofi', '-dmenu', '-i', '-p', 'Select:', '-format', 'i']
+    rofi_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    selected, error = rofi_process.communicate('\n'.join(keyvalues))
+    if rofi_process.returncode != 0:
+        print(f"Rofi error: {error}")
+        return None
+    index = int(selected)
+    return list(mapping.keys())[index]
+
+
+def main():
+    maps = parse()
+    lhs = select_mapping(maps["n"])
+    print(lhs, maps["n"][lhs])
 
 
 if __name__ == "__main__":
