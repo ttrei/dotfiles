d8a6e9f0dbb5a06065eece5220732382511c4cea
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-02-06 00:01:34 +0200

    note, format

--- keybinds/parse-nvim-keybinds.py
+++ keybinds/parse-nvim-keybinds.py
@@ -3,6 +3,7 @@
 
 import subprocess
 
+
 def parse():
     maps = {
         "n": {},
@@ -27,9 +28,11 @@ def select_mapping(mapping):
     keyvalues = []
     for key, value in mapping.items():
         keyvalues.append(f"{key}: {value}")
-    command = ['rofi', '-dmenu', '-i', '-p', 'Select:', '-format', 'i']
-    rofi_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    selected, error = rofi_process.communicate('\n'.join(keyvalues))
+    command = ["rofi", "-dmenu", "-i", "-p", "Select:", "-format", "i"]
+    rofi_process = subprocess.Popen(
+        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+    selected, error = rofi_process.communicate("\n".join(keyvalues))
     if rofi_process.returncode != 0:
         print(f"Rofi error: {error}")
         return None
