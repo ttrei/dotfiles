30b0ded9fd99d443373757066081f320440ec12b
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-02-07 23:55:46 +0200

    wip - iterfzf for selecting keybinds

--- keybinds/parse-nvim-keybinds.py
+++ keybinds/parse-nvim-keybinds.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import iterfzf
+
 
 import subprocess
 
@@ -28,14 +30,11 @@ def select_mapping(mapping):
     keyvalues = []
     for key, value in mapping.items():
         keyvalues.append(f"{key}: {value}")
-    command = ["rofi", "-dmenu", "-i", "-p", "Select:", "-format", "i"]
-    rofi_process = subprocess.Popen(
-        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-    )
-    selected, error = rofi_process.communicate("\n".join(keyvalues))
-    if rofi_process.returncode != 0:
-        print(f"Rofi error: {error}")
-        return None
+    # command = ["rofi", "-dmenu", "-i", "-p", "Select:", "-format", "i"]
+    # command = ["fzf", "--prompt=Select: ", "--height=40%", "--border"]
+    input_str = "\n".join(keyvalues)
+    result = iterfzf.iterfzf(keyvalues)
+    # get the index when using fzf: https://github.com/junegunn/fzf/issues/1660
     index = int(selected)
     return list(mapping.keys())[index]
 
