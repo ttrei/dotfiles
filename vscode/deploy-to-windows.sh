ffea4d6016e2f3572603ff5d112f0fcb89e3f443
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-05-20 10:55:43 +0300

    merge windows vscode config

---
+++ vscode/deploy-to-windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+config_dir=/mnt/c/Users/taukvrei/AppData/Roaming/Code/User
+cp keybindings.json $config_dir
+cp settings.json $config_dir
---
+++ vscode/get-from-windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+config_dir=/mnt/c/Users/taukvrei/AppData/Roaming/Code/User
+cp $config_dir/keybindings.json .
+cp $config_dir/settings.json .
