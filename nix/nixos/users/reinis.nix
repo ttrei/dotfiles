93656a0f3b7ba4d355b38990e77fda6134b43526
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-07-24 00:28:26 +0300

    Add user to dialout group to allow uploading firmware to esp32

--- nix/nixos/users/reinis.nix
+++ nix/nixos/users/reinis.nix
@@ -6,7 +6,7 @@
   users.users.reinis = {
     uid = 1000;
     isNormalUser = true;
-    extraGroups = ["networkmanager" "transmission" "wheel" "audio" "realtime" "input"];
+    extraGroups = ["networkmanager" "transmission" "wheel" "audio" "realtime" "input" "dialout"];
     # mkpasswd -m sha-512
     hashedPassword = "$6$Cg9okSaCqojkhWfc$/EOP6PeEaL5DPnjjNtG7k7.80O5X8Sc4Q2qiTQzS1n6vn.DTp3fI5dafofJzbU/MwFTnroSMg3CT8towxuUwG.";
   };
