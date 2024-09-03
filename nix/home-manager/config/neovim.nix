7e99cc9c16e2dd3d44fdb4f9fec793bde71ae5a6
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-09-03 18:43:44 +0300

    comment

--- nix/home-manager/config/neovim.nix
+++ nix/home-manager/config/neovim.nix
@@ -30,8 +30,9 @@
       };
       # NOTE(2024-09-03): avante.nvim needs to build some stuff with :AvanteBuild
       # I patched the source to build it in a tmp directory but that didn't help.
-      # It seems to expect that the build is executed in the plugin directory, but that's not possible if the plugin is
-      # located in nix store.
+      # Had to install cargo, pkg-config, openssl and export PKG_CONFIG_PATH=/nix/store/...-openssl-3.0.14-dev/lib/pkgconfig
+      # avante-nvim seems to expect that the build is executed in the plugin directory, but that's not possible if the
+      # plugin is located in the nix store.
       # TODO: Check out if nixvim will get the avante plugin: https://github.com/nix-community/nixvim/issues/2096
       avante-nvim = pkgs.vimUtils.buildVimPlugin {
           name = "avante-nvim";
