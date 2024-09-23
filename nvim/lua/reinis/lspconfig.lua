9fd05bf1841c39293768159f457009febab0d747
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-09-23 12:28:48 +0300

    tsserver has been deprecated in favor of ts_ls

--- nvim/lua/reinis/lspconfig.lua
+++ nvim/lua/reinis/lspconfig.lua
@@ -46,7 +46,7 @@ require("lspconfig").pyright.setup({
   on_attach = on_attach,
   flags = lsp_flags,
 })
-require("lspconfig").tsserver.setup({
+require("lspconfig").ts_ls.setup({
   on_attach = on_attach,
   flags = lsp_flags,
 })
