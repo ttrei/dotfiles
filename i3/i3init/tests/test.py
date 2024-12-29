a638416a4f9c6d3451453da60aa8902180d73ea9
Reinis Taukulis <reinis.taukulis@gmail.com>
2024-12-30 00:43:48 +0200

    Make i3init more reliable

--- i3/i3init/tests/test.py
+++ i3/i3init/tests/test.py
@@ -2,34 +2,24 @@
 import os
 
 import i3init
-
-TIMEOUT = 2.0
+from i3init import Program, Workspace
 
 HOME = os.path.expanduser("~")
 
-WORKSPACE_PROGRAMS = {
-    # workspace
-    "100:test": [
-        # exec_list, window_name, window_class, window_handling_commands
-        [
-            f"exec-in-dir {HOME}/dotfiles zutty -e sleep 3".split(),
-            "zutty-test",
-            "zutty",
-            ["move left"],
-        ],
-        [
-            f"exec-in-dir {HOME}/dotfiles zutty -e sleep 3".split(),
-            "zutty-test",
-            "zutty",
-            ["move to workspace 101:test"],
-        ],
-        # [f"code {HOME}/.config/vscode-workspaces/ziglings.code-workspace".split(), "visual studio code", "code", None],
-        # [f"code {HOME}/.config/vscode-workspaces/algorithms.code-workspace".split(), "visual studio code", "code", None],
-    ],
-    # "200:dev": [
-    #     ["zutty".split(), "zutty-dev", "zutty", None],
-    #     ["zutty".split(), "zutty-dev", "zutty", None],
-    # ],
-}
+workspace1 = Workspace("100:test").with_programs(
+    Program(
+        exec_cmd=f"exec-in-dir {HOME}/dotfiles zutty -e sleep 999999",
+        window_handling_commands=["move left"],
+    ),
+    Program(
+        exec_cmd=f"exec-in-dir {HOME}/dotfiles zutty -e sleep 3",
+        window_handling_commands=["move to workspace 101:test"],
+    ),
+)
 
-i3init.run(WORKSPACE_PROGRAMS, TIMEOUT)
+workspace2 = Workspace("200:dev").with_programs(
+    Program(exec_cmd="ghostty"),
+    Program(exec_cmd="zutty"),
+)
+
+i3init.run([workspace1, workspace2], timeout=2.0)
