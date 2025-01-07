25ff61276ee4f48788815d9cf72c29dc0b8a6d74
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-07 10:47:45 +0200

    separate ghostty config for wsl

--- bin/stage.sh
+++ bin/stage.sh
@@ -77,6 +77,9 @@ ln -s "$DOTFILES/terminal/starship.toml" "$STAGINGDIR/.config/starship.toml"
 ln -s "$DOTFILES/terminal/tmux.conf" "$STAGINGDIR/.tmux.conf"
 ln -s "$DOTFILES/terminal/xfce4terminalrc" "$STAGINGDIR/.config/xfce4/terminal/terminalrc"
 ln -s "$DOTFILES/terminal/ghostty" "$STAGINGDIR/.config/ghostty/config"
+if [ "$EXECUTION_ENV" = "wsl" ]; then
+    ln -sf "$DOTFILES/terminal/ghostty-wsl" "$STAGINGDIR/.config/ghostty/config"
+fi
 
 mkdir -p "$STAGINGDIR/.bash_aliases"
 ln -s "$DOTFILES/terminal/aliases/common" "$STAGINGDIR/.bash_aliases/common"
