2fdae11bc98d0e8c585c321ad3b5c06365f5a783
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-12 00:16:57 +0200

    switch avante to deepseek

--- bin/secrets.sh
+++ bin/secrets.sh
@@ -5,5 +5,6 @@ set -o nounset
 
 pass os-secrets/nix-github-token > "$HOME"/.nix-github-token
 pass os-secrets/anthropic-api-key > "$HOME"/.anthropic-api-key
+pass os-secrets/deepseek-api-key > "$HOME"/.deepseek-api-key
 pass os-secrets/openexchangerates-app-id > "$HOME"/.openexchangerates-app-id
 
--- nvim/lua/reinis/llm.lua
+++ nvim/lua/reinis/llm.lua
@@ -1,4 +1,15 @@
 ------ https://github.com/yetone/avante.nvim
 
 require('avante_lib').load()
-require('avante').setup ({})
+require('avante').setup ({
+  -- https://github.com/yetone/avante.nvim/wiki/Custom-providers#deepseek
+  provider = "deepseek",
+  vendors = {
+    deepseek = {
+      __inherited_from = "openai",
+      api_key_name = "DEEPSEEK_API_KEY",
+      endpoint = "https://api.deepseek.com",
+      model = "deepseek-coder",
+    },
+  },
+})
