f8262c9a4501905773e273d9b060fac552d85b0c
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-14 22:59:22 +0200

    Install llm

--- bootstrap/software/python/install-python-apps.sh
+++ bootstrap/software/python/install-python-apps.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+# shellcheck disable=SC1090
 . ~/.virtualenvs/misc/bin/activate
 python -m pip install -r "$SCRIPT_DIR/requirements.txt"
 python -m pip install --editable ~/dotfiles/i3/i3init
+
+uv tool install llm
