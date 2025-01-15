a7cb79e05c2aff553eda35dda5bfece477d4465a
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-15 10:18:28 +0200

    use uv to create the global venv

--- bootstrap/software/python/install-python-apps.sh
+++ bootstrap/software/python/install-python-apps.sh
@@ -2,11 +2,20 @@
 
 set -o errexit
 
+# Make sure we use the system python.
+# Don't want to create a virtualenv against a python from another virtualenv -
+# it may disappear.
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cd "$SCRIPT_DIR"
+direnv allow
+eval "$(direnv export bash)" || exit 1
+
+rm -rf ~/.virtualenvs/misc
+uv venv ~/.virtualenvs/misc
 
 # shellcheck disable=SC1090
 . ~/.virtualenvs/misc/bin/activate
-python -m pip install -r "$SCRIPT_DIR/requirements.txt"
-python -m pip install --editable ~/dotfiles/i3/i3init
+uv pip install -r "$SCRIPT_DIR/requirements.txt"
+uv pip install --editable ~/dotfiles/i3/i3init
 
 uv tool install llm
