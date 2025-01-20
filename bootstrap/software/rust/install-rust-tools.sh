6294afbf26f3253bb8be6a61b51e954bb36071c0
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-20 22:08:02 +0200

    Remove deprecated install scripts

--- bootstrap/software/rust/install-rust-tools.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-cargo install $(cat rust-tools.txt)
--- bootstrap/software/rust/install-rust.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# https://www.rust-lang.org/tools/install
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
