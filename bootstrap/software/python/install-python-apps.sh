112143444ae988cd70df10d1d53c0cdaede1090d
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-01-24 00:04:30 +0200

    upgrade python tools

--- bootstrap/software/python/install-python-apps.sh
+++ bootstrap/software/python/install-python-apps.sh
@@ -14,11 +14,12 @@ set -o errexit
 rm -rf ~/.virtualenvs/misc
 uv venv ~/.virtualenvs/misc
 
-uv tool install ~/dotfiles/i3/i3init
-uv tool install yt-dlp
-uv tool install llm
+uv tool install --upgrade ~/dotfiles/i3/i3init
+uv tool install --upgrade yt-dlp
+uv tool install --upgrade llm
+llm install llm-cmd
 
 if ! [ -d /tmp/chimeracat ]; then
     git clone https://github.com/scottvr/chimeracat.git /tmp/chimeracat
 fi
-uv tool install /tmp/chimeracat
+uv tool install --upgrade /tmp/chimeracat
