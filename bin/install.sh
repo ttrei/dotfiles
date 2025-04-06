f059e5bb872c53f325077c0ea507cc55f10fc59e
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-04-06 16:47:02 +0300

    simplify

--- bin/install.sh
+++ bin/install.sh
@@ -2,18 +2,6 @@
 
 set -o errexit
 
-# NOTE: maybe this is not needed when using uv
-# # Make sure we use the system python.
-# # Don't want to create a virtualenv against a python from another virtualenv -
-# # it may disappear.
-# SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-# cd "$SCRIPT_DIR"
-# direnv allow
-# eval "$(direnv export bash)" || exit 1
-
-rm -rf ~/.virtualenvs/misc
-uv venv ~/.virtualenvs/misc
-
 uv tool install --reinstall ~/dotfiles/i3/i3init
 uv tool install --upgrade yt-dlp
 uv tool install --upgrade llm
