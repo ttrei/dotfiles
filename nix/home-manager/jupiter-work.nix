0b8937389ebfca9e23af250de6f3618f11c5f8a6
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-05-08 13:56:15 +0300

    debeaver and stern for work

--- nix/home-manager/jupiter-work.nix
+++ nix/home-manager/jupiter-work.nix
@@ -11,12 +11,14 @@
   ];
 
   home.packages = with pkgs; [
+    dbeaver-bin
     jetbrains.idea-community-bin
     jetbrains.pycharm-community-bin
     kubectl
     kubelogin-oidc
     k9s
     python310Packages.pgsanity
+    stern
   ];
 
   programs.bash.enable = false;
