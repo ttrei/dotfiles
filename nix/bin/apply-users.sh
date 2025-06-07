ffbbb74150bcfc6e90581153654019b3b61cb878
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-06-07 13:56:19 +0300

    fixup

--- nix/bin/apply-users.sh
+++ nix/bin/apply-users.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-command="sudo nixos-rebuild switch --flake $HOME/dotfiles#reinis@$(hostname)"
+command="home-manager switch --flake $HOME/dotfiles#reinis@$(hostname)"
 
 echo "$command"
 eval "$command"
