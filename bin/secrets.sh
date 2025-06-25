c53ec926f50815519305737203a9224e90c91246
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-06-25 15:56:11 +0300

    add openrouter api key

--- bin/secrets.sh
+++ bin/secrets.sh
@@ -5,6 +5,7 @@ set -o nounset
 
 pass os-secrets/nix-github-token > "$HOME"/.nix-github-token
 pass os-secrets/anthropic-api-key > "$HOME"/.anthropic-api-key
+pass os-secrets/openrouter-api-key > "$HOME"/.openrouter-api-key
 pass os-secrets/deepseek-api-key > "$HOME"/.deepseek-api-key
 pass os-secrets/openexchangerates-app-id > "$HOME"/.openexchangerates-app-id
 
