d152f0f834926d651d7d493d141e3d18d2ad57c0
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-07-18 11:19:07 +0300

    remove chimeracat

--- bin/install.sh
+++ bin/install.sh
@@ -7,14 +7,5 @@ uv tool install --upgrade yt-dlp
 uv tool install --upgrade llm
 uv tool install --upgrade files-to-prompt
 
-if ! [ -d /tmp/chimeracat ]; then
-    git clone https://github.com/scottvr/chimeracat.git /tmp/chimeracat
-else
-    pushd /tmp/chimeracat
-    git pull
-    popd
-fi
-uv tool install --reinstall /tmp/chimeracat
-
 # https://llm.datasette.io/en/stable/plugins/directory.html
 llm install --force-reinstall llm-cmd llm-deepseek llm-claude-3
