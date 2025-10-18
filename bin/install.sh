0557dbd9765397ced40f15dafbfa9d28c5fc254b
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-10-18 23:07:16 +0300

    install beads in gemini docker image

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o nounset
+
+DOTFILES="$( cd "$(dirname "$0")/.." || exit >/dev/null 2>&1 ; pwd -P )"
 
 uv tool install --reinstall ~/dotfiles/i3/i3init
 uv tool install --upgrade yt-dlp
@@ -9,3 +12,5 @@ uv tool install --upgrade files-to-prompt
 
 # https://llm.datasette.io/en/stable/plugins/directory.html
 llm install --force-reinstall llm-cmd llm-deepseek llm-claude-3
+
+sudo docker build -f "$DOTFILES/llm/Dockerfile-gemini" -t gemini ./llm
--- llm/gemini-docker.sh
+++ llm/gemini-docker.sh
@@ -5,13 +5,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# Check latest tag here
-# https://console.cloud.google.com/artifacts/docker/gemini-code-dev/us/gemini-cli/sandbox
-IMAGE=us-docker.pkg.dev/gemini-code-dev/gemini-cli/sandbox:0.10.0-preview.1
+IMAGE=gemini
 
 sudo docker run --rm -it \
     -v "$HOME/.gemini:/home/node/.gemini" \
     -v "$HOME/.gemini-api-key:/home/node/.gemini/.env" \
     -v "$1":/home/node/code \
     $IMAGE \
-    /bin/bash -c "cd /home/node/code && gemini"
+    /bin/bash
