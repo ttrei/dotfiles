3b91cfc9d160f6e3eb1ee053089726b2a9d697e8
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-05 09:34:29 +0200

    run docker without sudo

--- nix/nixos/users/reinis.nix
+++ nix/nixos/users/reinis.nix
@@ -15,6 +15,7 @@
       "realtime"
       "input"
       "dialout"
+      "docker"
     ];
     # mkpasswd -m sha-512
     hashedPassword = "$6$Cg9okSaCqojkhWfc$/EOP6PeEaL5DPnjjNtG7k7.80O5X8Sc4Q2qiTQzS1n6vn.DTp3fI5dafofJzbU/MwFTnroSMg3CT8towxuUwG.";
