ebced7ebe07a31c851ec39d94610c34c016b1846
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-05 07:09:42 +0200

    jupiter-work networking

--- nix/nixos/jupiter-work.nix
+++ nix/nixos/jupiter-work.nix
@@ -19,16 +19,59 @@
 
   networking.hostName = "jupiter-work";
 
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "192.168.50.1/32" ];
-    listenPort = 51820;
+  # Forward corporate ip ranges via wireguard to the OpenVPN tunnel on mercury.
+  networking.wg-quick.interfaces.wg0 = {
+    autostart = true;
+    address = [ "192.168.50.1/32" ];
     privateKeyFile = "/root/wireguard-keys/mercury.key";
+    dns = [ "10.57.6.3" ]; # Found with `resolvectl status` on mercury.
     peers = [
       {
         publicKey = "8BoqwfxDDRXROjpkdt595bV6urQw1VrltrvOAUuO2lA=";
-        allowedIPs = [ "192.168.50.2/32" ];
         endpoint = "mercury:51820";
         persistentKeepalive = 25;
+        # Find the ip ranges to forward (on mercury with OpenVPN up):
+        # $ ip route show dev tun0
+        # 10.57.3.0/24 via 192.168.226.1
+        # 10.57.5.0/24 via 192.168.226.1
+        # 10.57.6.0/24 via 192.168.226.1
+        # 10.57.8.0/24 via 192.168.226.1
+        # 10.57.9.0/24 via 192.168.226.1
+        # 10.57.10.0/24 via 192.168.226.1
+        # 10.57.11.0/24 via 192.168.226.1
+        # 10.57.12.0/23 via 192.168.226.1
+        # 10.57.14.0/24 via 192.168.226.1
+        # 10.57.68.0/24 via 192.168.226.1
+        # 10.57.200.0/24 via 192.168.226.1
+        # 10.57.201.0/24 via 192.168.226.1
+        # 10.57.206.0/24 via 192.168.226.1
+        # 10.57.210.0/24 via 192.168.226.1
+        # 10.99.0.0/16 via 192.168.226.1
+        # 10.100.0.0/16 via 192.168.226.1
+        # 172.27.5.0/24 via 192.168.226.1
+        # 172.27.200.0/24 via 192.168.226.1
+        # 192.168.226.0/24 proto kernel scope link src 192.168.226.45
+        allowedIPs = [
+          "10.57.3.0/24"
+          "10.57.5.0/24"
+          "10.57.6.0/24"
+          "10.57.8.0/24"
+          "10.57.9.0/24"
+          "10.57.10.0/24"
+          "10.57.11.0/24"
+          "10.57.12.0/23"
+          "10.57.14.0/24"
+          "10.57.68.0/24"
+          "10.57.200.0/24"
+          "10.57.201.0/24"
+          "10.57.206.0/24"
+          "10.57.210.0/24"
+          "10.99.0.0/16"
+          "10.100.0.0/16"
+          "172.27.5.0/24"
+          "172.27.200.0/24"
+          "192.168.226.0/24"
+        ];
       }
     ];
   };
