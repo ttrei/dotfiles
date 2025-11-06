750d812cb951b2f6ffd7901c482cf6b39d5588e9
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-06 17:15:29 +0200

    jira access token

--- bin/secrets.sh
+++ bin/secrets.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o nounset
 
+# shellcheck source=/dev/null
+. "$HOME/.dotfiles-env"
+
 pass os-secrets/nix-github-token > "$HOME"/.nix-github-token
 pass os-secrets/anthropic-api-key > "$HOME"/.anthropic-api-key
 pass os-secrets/openrouter-api-key > "$HOME"/.openrouter-api-key
@@ -12,3 +15,7 @@ pass os-secrets/openexchangerates-app-id > "$HOME"/.openexchangerates-app-id
 
 mkdir -p "$HOME"/.config/io.datasette.llm
 pass other/llm-keys.json > "$HOME"/.config/io.datasette.llm/keys.json
+
+if [ "$CONTEXT" = "work" ]; then
+    pass tieto2/jira-personal-access-token > "$HOME"/.jira-access-token
+fi
