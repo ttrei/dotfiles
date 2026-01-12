18bd050d90229cff5f159b996de5b88e2c339a51
Reinis Taukulis <reinis.taukulis@gmail.com>
2026-01-12 23:20:18 +0200

    rename

--- nix/bin/apply-system.sh
+++ nix/bin/apply-system.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-command="sudo nixos-rebuild switch --flake $HOME/dotfiles#$(hostname)"
+command="sudo nixos-rebuild switch --flake $HOME/dev/dotfiles#$(hostname)"
 
 echo "$command"
 eval "$command"
--- nix/bin/apply-users.sh
+++ nix/bin/apply-users.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-command="home-manager switch --flake $HOME/dotfiles#reinis@$(hostname)"
+command="home-manager switch --flake $HOME/dev/dotfiles#reinis@$(hostname)"
 
 echo "$command"
 eval "$command"
