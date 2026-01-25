37df4fa457228904109e88949de1fe51b78724ba
Reinis Taukulis <reinis.taukulis@gmail.com>
2026-01-25 13:06:05 +0200

    allow gpu acceleration in qemu

--- nix/nixos/hardware-configurations/jupiter.nix
+++ nix/nixos/hardware-configurations/jupiter.nix
@@ -131,5 +131,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  hardware.opengl.enable = true;
+
   hardware.bluetooth.enable = true;
 }
